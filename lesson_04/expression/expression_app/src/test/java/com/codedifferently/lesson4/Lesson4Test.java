5278b506c6ad12cfdb9fe8cdbd836269bcbf56a5
Anthony D. Mays <anthony@morganlatimer.com>
2024-03-02 09:43:27 -0800

    chore: removes outdated app settings
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>

--- lesson_04/expression/expression_app/src/test/java/com/codedifferently/lesson4/Lesson4Test.java
+++ lesson_04/expression/expression_app/src/test/java/com/codedifferently/lesson4/Lesson4Test.java
@@ -1,4 +1,4 @@
-package com.codedifferently.lesson2;
+package com.codedifferently.lesson4;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,28 +20,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
-@ContextConfiguration(classes = Lesson2.class)
+@ContextConfiguration(classes = Lesson4.class)
 @ExtendWith(SoftAssertionsExtension.class)
-class Lesson2Test {
-
-  @Autowired private QuizAnswers quizAnswers;
-
-  private List<QuizQuestion> quizQuestions;
+class Lesson4Test {
 
   private SoftAssertions softly;
 
-  private final int EXPECTED_NUMBER_OF_QUESTIONS = 11;
-
   @BeforeEach
   void setUp() {
-    getQuestions();
     softly = new SoftAssertions();
-  }
-
-  private void getQuestions() {
-    var questions = new ArrayList<QuizQuestion>(Lesson2.makeQuizQuestions());
-    questions.sort(Comparator.comparingInt(QuizQuestion::getQuestionNumber));
-    quizQuestions = questions;
   }
 
   @AfterEach
@@ -49,52 +36,4 @@ class Lesson2Test {
     softly.assertAll();
   }
 
-  @Test
-  void checkQuizQuestions_areAssembledCorrectly() {
-    // Expect the right number of questions.
-    assertThat(quizQuestions.size())
-        .as("Check # of questions")
-        .isEqualTo(EXPECTED_NUMBER_OF_QUESTIONS);
-
-    // Expect questions to be numbered correctly.
-    for (int i = 0; i < quizQuestions.size(); i++) {
-      assertThat(quizQuestions.get(i).getQuestionNumber())
-          .as("Check question number is correct")
-          .isEqualTo(i);
-    }
-  }
-
-  @Test
-  void checkQuizQuestions_promptsAreUnique() {
-    Set<String> questionPrompts =
-        quizQuestions.stream().map(QuizQuestion::getQuestionPrompt).collect(Collectors.toSet());
-    assertEquals(
-        EXPECTED_NUMBER_OF_QUESTIONS,
-        questionPrompts.size(),
-        "Expected "
-            + EXPECTED_NUMBER_OF_QUESTIONS
-            + " unique questions but got "
-            + questionPrompts.size());
-  }
-
-  @Test
-  void checkQuestions_answeredCorrectly() {
-    assertThat(quizAnswers.size()).as("Check # of answers").isEqualTo(quizQuestions.size());
-
-    for (QuizQuestion question : quizQuestions) {
-      String actualAnswer = question.getAnswer();
-
-      // Check that the question was answered.
-      softly
-          .assertThat(actualAnswer)
-          .as("Question " + question.getQuestionNumber() + " must be answered")
-          .isNotEqualTo(AnswerChoice.UNANSWERED);
-
-      // Check that the answer is correct.
-      softly
-          .assertThat(quizAnswers.checkAnswer(question.getQuestionNumber(), actualAnswer))
-          .as("Checking answer matches correct answer: " + question.getQuestionPrompt())
-          .isTrue();
-    }
-  }
 }
