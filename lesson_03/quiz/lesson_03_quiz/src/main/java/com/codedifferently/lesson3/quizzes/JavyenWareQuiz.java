d745a19e9a7220c5b7e383a730124279653b2f50
Your Name <you@example.com>
2024-03-03 19:30:31 +0000

    chore: updated JavyenWareQuiz.java file and added my encrypted answers to the application.yml file

---
+++ lesson_03/quiz/lesson_03_quiz/src/main/java/com/codedifferently/lesson3/quizzes/JavyenWareQuiz.java
@@ -0,0 +1,43 @@
+package com.codedifferently.lesson3.quizzes;
+
+import com.codedifferently.instructional.quiz.AnswerChoice;
+import com.codedifferently.instructional.quiz.MultipleChoiceQuizQuestion;
+import com.codedifferently.instructional.quiz.QuizQuestion;
+import com.codedifferently.instructional.quiz.QuizQuestionProvider;
+import java.util.List;
+import java.util.Map;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JavyenWareQuiz implements QuizQuestionProvider {
+
+  public String getProviderName() {
+    return "javyenware";
+  }
+
+  public List<QuizQuestion> makeQuizQuestions() {
+    return List.of(makeQuestion0(), makeQuestion1());
+  }
+
+  private QuizQuestion makeQuestion0() {
+    return new MultipleChoiceQuizQuestion(
+        0,
+        "What is Object Oriented Programming?",
+        Map.of(
+            AnswerChoice.A,
+                "A programming language model organized around objects rather than actions, and data rather than logic.",
+            AnswerChoice.B,
+                "A programming convetion that only allows for the use of specific Objects.",
+            AnswerChoice.C, "Using Objects to program.",
+            AnswerChoice.D,
+                "A paradigm of building computer programs using expressions and functions without mutating state and data."),
+        AnswerChoice.UNANSWERED); // Replace `UNANSWERED` with the correct answer.
+  }
+
+  private QuizQuestion makeQuestion1() {
+    return new QuizQuestion(
+        1,
+        "What is the defintion of a class in the Java programming language?",
+        ""); // Provide an answer.
+  }
+}
