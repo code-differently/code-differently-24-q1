501a49f190bdab9030bd9e2b48d1f5c1c65fb215
RichHawkins <RichHawkins1986@gmail.com>
2024-03-04 22:32:36 +0000

    test

--- lesson_03/quiz/lesson_03_quiz/src/main/java/com/codedifferently/lesson3/quizzes/RichHawkinsQuiz.java
+++ lesson_03/quiz/lesson_03_quiz/src/main/java/com/codedifferently/lesson3/quizzes/RichHawkinsQuiz.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class RichHawkinsQuiz implements QuizQuestionProvider {
 
-  public String getProviderName() {                               
+  public String getProviderName() {
     return "richhawkins";
   }
 
