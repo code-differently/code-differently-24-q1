0fb2c1c83de4eef5ae5c0b3a5b1db0010c932c38
RandyCastr0 <randyyc15@gmail.com>
2024-03-04 15:42:00 +0000

    added 2 questions

--- lesson_03/quiz/lesson_03_quiz/src/main/java/com/codedifferently/lesson3/quizzes/RandycastroQuiz.java
+++ lesson_03/quiz/lesson_03_quiz/src/main/java/com/codedifferently/lesson3/quizzes/RandycastroQuiz.java
@@ -1,2 +1,27 @@
 package com.codedifferently.lesson3.quizzes;
 
+public String getProviderName() {
+    return "RandyCastroQuiz";
+}
+
+public class RandyCastroQuiz {
+    
+    // Your quiz questions and answers will be implemented here. 
+
+
+
+
+Question 0:
+
+"What is the binary representation of the decimal number 10"?
+A) 1010
+B) 1001
+C) 1100
+D) 0110
+Question 1:
+
+"What component of a computer is responsible for excuting instructions"?
+A) "CPU (Central Processing Unit)"
+B) "GPU (Graphics Processing Unit)""
+C) "RAM (Random Access Memory)""
+D) "HDD (Hard Disk Drive)""
