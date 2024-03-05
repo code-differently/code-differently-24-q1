325a6facd5069daab0d2c60a55fc6a9f2385f798
Kenrixkk22 <Ksutherland@Thecodesociety.onmicrosoft.com>
2024-03-05 20:02:03 +0000

    fixesquiz

--- lesson_03/quiz/lesson_03_quiz/src/main/java/com/codedifferently/lesson3/quizzes/KenrickSutherlandQuiz.java
+++ lesson_03/quiz/lesson_03_quiz/src/main/java/com/codedifferently/lesson3/quizzes/KenrickSutherlandQuiz.java
@@ -1,6 +1,4 @@
-
 package com.codedifferently.lesson3.quizzes;
-
 
 import com.codedifferently.instructional.quiz.AnswerChoice;
 import com.codedifferently.instructional.quiz.MultipleChoiceQuizQuestion;
@@ -10,46 +8,38 @@ import java.util.List;
 import java.util.Map;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class KenrickSutherlandQuiz implements QuizQuestionProvider {
 
+  public String getProviderName() {
+    return "kenricksutherland";
+  }
 
- public String getProviderName() {
-   return "kenricksutherland";
- }
+  public List<QuizQuestion> makeQuizQuestions() {
+    return List.of(makeQuestion0(), makeQuestion1());
+  }
 
+  private QuizQuestion makeQuestion0() {
+    return new MultipleChoiceQuizQuestion(
+        0,
+        "What is the hexadecimal representation of the decimal number 255?",
+        Map.of(
+            AnswerChoice.A, "EE",
+            AnswerChoice.B, "CC",
+            AnswerChoice.C, "FF",
+            AnswerChoice.D, "DD"),
+        AnswerChoice.UNANSWERED); // Replace `UNANSWERED` with the correct answer.
+  }
 
- public List<QuizQuestion> makeQuizQuestions() {
-   return List.of(makeQuestion0(), makeQuestion1());
- }
-
-
- private QuizQuestion makeQuestion0() {
-   return new MultipleChoiceQuizQuestion(
-       0,
-       "What is the hexadecimal representation of the decimal number 255?",
-       Map.of(
-           AnswerChoice.A, "EE",
-           AnswerChoice.B, "CC",
-           AnswerChoice.C, "FF",
-           AnswerChoice.D, "DD"),
-       AnswerChoice.UNANSWERED); // Replace `UNANSWERED` with the correct answer.
- }
-
-
- private QuizQuestion makeQuestion1() {
-   return new MultipleChoiceQuizQuestion(
-       1,
-       "Which component of a computer is primarily responsible for executing instructions and performing calculations??",
-       Map.of(
-           AnswerChoice.A, "CPU",
-           AnswerChoice.B, "RAM",
-           AnswerChoice.C, "GPU",
-           AnswerChoice.D, "SSD"),
-       AnswerChoice.UNANSWERED); // Replace `UNANSWERED` with the correct answer.
- }
+  private QuizQuestion makeQuestion1() {
+    return new MultipleChoiceQuizQuestion(
+        1,
+        "Which component of a computer is primarily responsible for executing instructions and performing calculations??",
+        Map.of(
+            AnswerChoice.A, "CPU",
+            AnswerChoice.B, "RAM",
+            AnswerChoice.C, "GPU",
+            AnswerChoice.D, "SSD"),
+        AnswerChoice.UNANSWERED); // Replace `UNANSWERED` with the correct answer.
+  }
 }
-
-
-
