ef742f896cf6d9fe64adb5ac508f212dea47c7a5
cibezim <123107253+cibezim@users.noreply.github.com>
2024-03-06 21:38:02 +0000

    Updated class to have add, divide, and multiply functions

--- lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
+++ lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
@@ -8,13 +8,35 @@ public class ExpressionCalculator {
    * @param b
    * @return
    */
-  public double calculate(double a, double b, double c, double d, double e) {
-    // Implement your code here to return the correct value.
-    double sum = ((a + b) * c) / pow(d, e);
-    return sum;
+  public double add(double a, double b) {
+    double addSum = a + b;
+    return addSum;
+  }
+
+  public double multiply(double addSum, double c) {
+    double multiplySum = addSum * c;
+    return multiplySum;
+  }
+
+  public double divide(double multiplySum, double d) {
+    double divideSum = multiplySum / d;
+    return divideSum;
   }
 
   public double pow(double base, double exponent) {
     return Math.pow(base, exponent);
+  }
+
+  public double calculate(double a, double b, double c, double d, double e) {
+    // Implement your code here to return the correct value.
+    double sum = add(a, b);
+
+    double product = multiply(sum, c);
+
+    double power = pow(d, e);
+
+    double result = divide(product, power);
+
+    return result;
   }
 }
