e4fdb39ce54a5317cfdced5df3cb6a73a0d3a8ae
Your Name <warejavyen@gmail.com>
2024-03-07 19:08:32 +0000

    chore: updating ExpressionCalculator.java

--- lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
+++ lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
@@ -10,10 +10,20 @@ public class ExpressionCalculator {
    */
   public double calculate(double a, double b, double c, double d, double e) {
     // Implement your code here to return the correct value.
-    return 0d;
+
+  public double add(double a, double b){
+    return a + b;
+  }
+
+  public double multiply(double a, double b, double c){
+    return a+b * c;
   }
 
   public double pow(double base, double exponent) {
     return Math.pow(base, exponent);
+  }
+
+  public double divide(double a, double b, double c, double d){
+    
   }
 }
