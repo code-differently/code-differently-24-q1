1e6e8fe9bc7d1e4a92be6d047edfc5ddaa7b1952
SherllinT <s.torres91@gmail.com>
2024-03-07 19:32:01 +0000

    "ran spotless apply"

--- lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
+++ lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
@@ -9,27 +9,26 @@ public class ExpressionCalculator {
    * @return
    */
   public double calculate(double a, double b, double c, double d, double e) {
-    double output= ((a + b) * c) / pow(d, e);
+    double output = ((a + b) * c) / pow(d, e);
     return output;
   }
 
-  public double add (double a, double b) {
-    double sum = a + b; 
-    return sum; 
+  public double add(double a, double b) {
+    double sum = a + b;
+    return sum;
   }
-  
-  public double multiply (double a, double b) {
+
+  public double multiply(double a, double b) {
     double product = a * b;
-    return product; 
+    return product;
   }
-  public double divide (double a, double b){
+
+  public double divide(double a, double b) {
     double quotient = a / b;
     return quotient;
   }
-  
+
   public double pow(double base, double exponent) {
     return Math.pow(base, exponent);
   }
 }
-
-
