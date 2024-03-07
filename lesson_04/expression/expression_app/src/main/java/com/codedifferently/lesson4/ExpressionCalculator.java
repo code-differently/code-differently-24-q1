61ef869fb3f4bfe62858bb02988937661eb2c368
Nataya Price <nprice@thecodesociety.onmicrosoft.com>
2024-03-07 19:13:51 +0000

    fixed formatting #2

--- lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
+++ lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
@@ -1,9 +1,7 @@
 package com.codedifferently.lesson4;
 
 public class ExpressionCalculator {
-  /**
-   * Returns the calculation of ((a + b) * c) / d^e
-   */
+  /** Returns the calculation of ((a + b) * c) / d^e */
   public double calculate(double a, double b, double c, double d, double e) {
     // Implement your code here to return the correct value.
     double addition = add(a, b);
@@ -23,12 +21,12 @@ public class ExpressionCalculator {
     return product;
   }
 
-  public double pow(double base, double exponent) {
-    return Math.pow(base, exponent);
-  }
-
   public double divide(double product, double pow) {
     double quotient = product / pow;
     return quotient;
+  }
+
+  public double pow(double base, double exponent) {
+    return Math.pow(base, exponent);
   }
 }
