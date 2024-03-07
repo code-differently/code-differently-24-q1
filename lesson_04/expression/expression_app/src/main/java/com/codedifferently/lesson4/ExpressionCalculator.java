287338f8aca3fd652f5a67e98979d8cace9e41d7
Nataya Price <nprice@thecodesociety.onmicrosoft.com>
2024-03-07 16:57:27 +0000

    added 3 functions

--- lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
+++ lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
@@ -8,12 +8,42 @@ public class ExpressionCalculator {
    * @param b
    * @return
    */
-  public double calculate(double a, double b, double c, double d, double e) {
-    // Implement your code here to return the correct value.
-    return 0d;
+
+  /* add a+b
+   * multiply a+b by c
+   * figure out exponent
+   * divide product
+   */
+
+  public double add(double a, double b) {
+    double sum = a + b;
+    return sum;
+  }
+
+  public double multiply(double sum, double c) {
+    double product = sum * c;
+    return product;
   }
 
   public double pow(double base, double exponent) {
     return Math.pow(base, exponent);
   }
+
+  public double divide(double product, double pow) {
+    double quotient = product / pow;
+    return quotient;
+  }
+
+  public double calculate(double a, double b, double c, double d, double e) {
+    // Implement your code here to return the correct value.
+    double addition = add(a, b);
+    double multiplication = multiply(addition, c);
+    double power = pow(d, e);
+    double division = divide(multiplication, power);
+    return division;
+  }
+
+  /* public double pow(double base, double exponent) {
+    return Math.pow(base, exponent);
+  }*/
 }
