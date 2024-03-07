1aa449bc13c272c671a2b1df9e8f8c486f58cd04
Mohamed <moibrahi7@gmail.com>
2024-03-07 13:58:00 +0000

    Fix: added add, multipy, and divide to ensure code runs properly

--- lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
+++ lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
@@ -9,8 +9,22 @@ public class ExpressionCalculator {
    * @return
    */
   public double calculate(double a, double b, double c, double d, double e) {
-    // Implement your code here to return the correct value.
-    return 0d;
+    return divide(multiply(add(a, b), c), pow(d, e));
+  }
+
+  public double add(double z, double y) {
+    double sum = z + y;
+    return sum;
+  }
+
+  public double multiply(double z, double y) {
+    double product = z * y;
+    return product;
+  }
+
+  public double divide(double z, double y) {
+    double quotient = z / y;
+    return quotient;
   }
 
   public double pow(double base, double exponent) {
