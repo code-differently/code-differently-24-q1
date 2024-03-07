14a5b5f72342790ef86ac673f7e383ba330b5cbb
Kevin Mason <kevinmason@Codes-MacBook-Air-83.local>
2024-03-07 15:30:52 -0500

    Added ExpressionCalculator.java

--- lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
+++ lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
@@ -8,12 +8,39 @@ public class ExpressionCalculator {
    * @param b
    * @return
    */
-  public double calculate(double a, double b, double c, double d, double e) {
-    // Implement your code here to return the correct value.
-    return 0d;
+  public double add(double a, double b) {
+    double addAnswer = a + b;
+    return addAnswer;
+  }
+
+  public double multiply(double addAnswer, double c) {
+    double multiplyAnswer = addAnswer * c;
+    return multiplyAnswer;
   }
 
   public double pow(double base, double exponent) {
     return Math.pow(base, exponent);
+  }
+
+  public double divide(double multiplyAnswer, double d) {
+    double divideAnswer = multiplyAnswer / d;
+    return divideAnswer;
+  }
+
+  public double calculate(double a, double b, double c, double d, double e) {
+    // Implement your code here to return the correct value.
+    double sum = add(a, b);
+
+    
+    double product = multiply(sum, c);
+
+  
+    double power = pow(d, e);
+
+  
+    double result = divide(product, power);
+
+  
+    return result;
   }
 }
