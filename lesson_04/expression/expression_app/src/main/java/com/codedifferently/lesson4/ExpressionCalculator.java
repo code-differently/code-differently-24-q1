2ef9813604e02dce3f927cced3a70dc9bc6111c2
RichHawkins <RichHawkins1986@gmail.com>
2024-03-07 15:24:20 +0000

    feat: adds math functions and solves equation

--- lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
+++ lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
@@ -9,11 +9,27 @@ public class ExpressionCalculator {
    * @return
    */
   public double calculate(double a, double b, double c, double d, double e) {
-    // Implement your code here to return the correct value.
-    return 0d;
+    double sum = add(a, b);
+    double result = (sum * c) / pow(d, e);
+    return result;
   }
 
   public double pow(double base, double exponent) {
     return Math.pow(base, exponent);
+  }
+
+  public double add(double a, double b) {
+    double sum = a + b;
+    return sum;
+  }
+
+  public double multiply(double a, double b) {
+    double factor = a * b;
+    return factor;
+  }
+
+  public double divide(double a, double b) {
+    double quotient = a / b;
+    return quotient;
   }
 }
