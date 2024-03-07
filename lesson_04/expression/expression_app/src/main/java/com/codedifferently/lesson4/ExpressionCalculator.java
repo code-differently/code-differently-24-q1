ab30116e6ed142361f35fc6080a5198d61f11592
Jordan C Eldridge <88390606+HeirJordan3@users.noreply.github.com>
2024-03-07 19:44:29 +0000

    adding exercise

--- lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
+++ lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
@@ -4,12 +4,40 @@ public class ExpressionCalculator {
   /**
    * Returns the calculation of ((a + b) * c) / d^e
    */
-  public double calculate(double a, double b, double c, double d, double e) {
-    // Implement your code here to return the correct value.
-    return 0d;
+
+  public double add(double a, double b){
+    double addSum = a + b;
+    return addSum;
+
+   }
+
+  public double multiply(double a, double b){
+    double product = a * b;
+    return product;
+
   }
+
+
+  public double divide(double a, double b){
+    double quotient = a / b;
+    return quotient;
+
+  }
+
+
+
 
   public double pow(double base, double exponent) {
     return Math.pow(base, exponent);
+  }
+
+  public double calculate(double a, double b, double c, double d, double e) {
+    double sum = add(a, b);
+    double productSum = multiply(a, b);
+    double power = pow(b, e);
+
+    double result = divide(productSum,power);
+    // Implement your code here to return the correct value.
+    return result;
   }
 }
