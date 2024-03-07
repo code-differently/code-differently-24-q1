3c03cbff7162bf4bf786646dcad3e36e78a7feff
Vicente Vigueras <vicente.viguerassilva@gmail.com>
2024-03-07 16:01:51 +0000

    Feat: Adds methods that help solve a mathematical equation.

--- lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
+++ lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
@@ -8,12 +8,26 @@ public class ExpressionCalculator {
    * @param b
    * @return
    */
-  public double calculate(double a, double b, double c, double d, double e) {
-    // Implement your code here to return the correct value.
-    return 0d;
+  public double add(double a, double b) {
+    return a + b;
+  }
+
+  public double multiply(double a, double b) {
+    return a * b;
   }
 
   public double pow(double base, double exponent) {
     return Math.pow(base, exponent);
+  }
+
+  public double divide(double numerator, double denominator) {
+    return numerator / denominator;
+  }
+
+  public double calculate(double a, double b, double c, double d, double e) {
+    double sum = add(a, b);
+    double multiplication = multiply(sum, c);
+    double exponential = pow(d, e);
+    return divide(multiplication, exponential);
   }
 }
