f7fbe7f225a5b76c0e22742616a9036bc1c772f4
CD-Jamira <jamirabailey@gmail.com>
2024-03-07 19:37:10 +0000

    updated formatting

--- lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
+++ lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
@@ -3,10 +3,6 @@ package com.codedifferently.lesson4;
 public class ExpressionCalculator {
   /**
    * Returns the calculation of ((a + b) * c) / d^e
-   *
-   * @param a
-   * @param b
-   * @return
    */
   public double calculate(double a, double b, double c, double d, double e) {
     // Implement your code here to return the correct value.
