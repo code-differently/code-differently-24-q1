7724f3917ab540a11909c553771ef74dca02c87a
Aaron2278 <aaronsantiago542@gmail.com>
2024-03-08 14:13:53 +0000

    fix: formatting

--- lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
+++ lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
@@ -3,7 +3,7 @@ package com.codedifferently.lesson4;
 public class ExpressionCalculator {
 
   public double calculate(double a, double b, double c, double d, double e) {
-    double result = divide(multiply(add(a, b), c), pow(d, e)); 
+    double result = divide(multiply(add(a, b), c), pow(d, e));
     return result;
   }
 
@@ -12,8 +12,7 @@ public class ExpressionCalculator {
   }
 
   public double divide(double a, double b) {
-    if (b == 0) {
-    }
+    if (b == 0) {}
     return a / b;
   }
 
@@ -22,6 +21,6 @@ public class ExpressionCalculator {
   }
 
   public double pow(double base, double exponent) {
-    return Math.pow(base, exponent); //gives base to the power of exponent 
+    return Math.pow(base, exponent); // gives base to the power of exponent
   }
 }
