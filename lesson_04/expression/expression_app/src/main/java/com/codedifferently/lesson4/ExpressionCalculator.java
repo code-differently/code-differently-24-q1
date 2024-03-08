c4a1acccaa3a32adce7599cbe1453747183eb9f3
Anthony D. Mays <anthony@morganlatimer.com>
2024-03-08 09:50:19 -0800

    chore: fix formatting
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>

--- lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
+++ lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
@@ -1,9 +1,7 @@
 package com.codedifferently.lesson4;
 
 public class ExpressionCalculator {
-  /**
-   * Returns the calculation of ((a + b) * c) / d^e
-   */
+  /** Returns the calculation of ((a + b) * c) / d^e */
   public double calculate(double a, double b, double c, double d, double e) {
     // Implement your code here to return the correct value.
     return 0d;
