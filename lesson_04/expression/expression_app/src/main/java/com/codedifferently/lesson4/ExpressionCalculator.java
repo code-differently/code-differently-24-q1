4fa337844ff60941901096d9b8f75814ecb39016
Your Name <warejavyen@gmail.com>
2024-03-08 14:53:35 +0000

    chore: added the addition, multiply, divide function to ExpressionCalculator.java

--- lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
+++ lesson_04/expression/expression_app/src/main/java/com/codedifferently/lesson4/ExpressionCalculator.java
@@ -1,29 +1,31 @@
 package com.codedifferently.lesson4;
 
 public class ExpressionCalculator {
-  /**
-   * Returns the calculation of ((a + b) * c) / d^e
-   */
+  /** Returns the calculation of ((a + b) * c) / d^e */
   public double calculate(double a, double b, double c, double d, double e) {
-    // Implement your code here to return the correct value.    
-    return 0;
+    // Implement your code here to return the correct value.
+    double addTotal = add(a, b);
+    double productTotal = multiply(addTotal, c);
+    double powTotal = pow(d, e);
+    double quotientTotal = divide(productTotal, powTotal);
+    return quotientTotal;
   }
 
-  // addition function 
-  public double add(double a, double b){
+  // addition function
+  public double add(double a, double b) {
     double sum = a + b;
     return sum;
   }
 
-  // multiply function 
-  public double multiply(double sum, double c){
+  // multiply function
+  public double multiply(double sum, double c) {
     double product = sum * c;
     return product;
   }
 
   // divide function
-  public double divide(double a , double b){
-    double quotient =  a / b;
+  public double divide(double numerator, double denominator) {
+    double quotient = numerator / denominator;
     return quotient;
   }
 
@@ -31,5 +33,4 @@ public class ExpressionCalculator {
   public double pow(double base, double exponent) {
     return Math.pow(base, exponent);
   }
-
-} 
+}
