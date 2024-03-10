8db111f124e182842d50f05851ea54db47f0a274
Anthony D. Mays <anthony@morganlatimer.com>
2024-03-09 18:13:50 -0800

    chore: refactors string function
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>

--- lesson_06/conditionals/conditionals_app/src/main/java/com/codedifferently/lesson6/Lesson6.java
+++ lesson_06/conditionals/conditionals_app/src/main/java/com/codedifferently/lesson6/Lesson6.java
@@ -31,34 +31,9 @@ public class Lesson6 {
    * @rerturn -1 if a is less than b, 1 if a is greater than b, and 0 otherwise.
    */
   public static int compareStrings(String a, String b) {
-    int distance = computeLexographicDistance(a, b);
+    int distance = Utils.computeLexographicDistance(a, b);
 
     // TODO(you): Finish this method.
-
-    return 0;
-  }
-
-  /**
-   * Computes a score describing the lexographic distance between two strings. For two strings of
-   * equal length, the score will be the difference between the first differing character. In the
-   * case where either is a sub-string of the other, then the difference in length determines the
-   * score.
-   *
-   * @param a The first string to compare.
-   * @param b The second string to compare.
-   * @return A score representing the lexographic distance between two strings.
-   */
-  private static int computeLexographicDistance(String a, String b) {
-    for (var i = 0; i < a.length(); ++i) {
-      if (a.charAt(i) == b.charAt(i)) {
-        continue;
-      }
-      return (int) a.charAt(i) - (int) b.charAt(i);
-    }
-
-    if (a.length() != b.length()) {
-      return a.length() - b.length();
-    }
 
     return 0;
   }
