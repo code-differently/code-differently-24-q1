0fdfb6ad103f3ba0a011241ebc4823d2633599fb
Your Name <warejavyen@gmail.com>
2024-03-10 01:08:11 +0000

    chore: fixed formating errors in JavvyenWareProvider.java

--- lesson_05/types/types_app/src/main/java/com/codedifferently/lesson5/dataprovider/JavyenWareProvider.java
+++ lesson_05/types/types_app/src/main/java/com/codedifferently/lesson5/dataprovider/JavyenWareProvider.java
@@ -5,11 +5,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JavyenWareProvider extends DataProvider {
-    public String getProviderName() {
-        return "anthonymays";
-    }
-    public Map<String, Class> getColumnTypeByName() {
-        return Map.of(
+  public String getProviderName() {
+    return "javyenware";
+  }
+
+  public Map<String, Class> getColumnTypeByName() {
+    return Map.of(
         "column1", String.class,
         "column2", Integer.class,
         "column3", Float.class,
@@ -17,5 +18,5 @@ public class JavyenWareProvider extends DataProvider {
         "column5", Double.class,
         "column6", Short.class,
         "column7", Long.class);
-    }
+  }
 }
