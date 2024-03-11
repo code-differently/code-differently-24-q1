40c3d961e300337e65d0199222a07eaf2b3dbd07
Aaron2278 <aaronsantiago542@gmail.com>
2024-03-11 13:16:05 +0000

    feat: Lesson_05 DataProvider implementation

---
+++ lesson_05/types/types_app/src/main/java/com/codedifferently/lesson5/dataprovider/AaronSantiagoProvider.java
@@ -0,0 +1,23 @@
+package com.codedifferently.lesson5.dataprovider;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AaronSantiagoProvider extends DataProvider {
+  public String getProviderName() {
+    return "aaronsantiago";
+  }
+
+  public Map<String, Class> getColumnTypeByName() {
+    return Map.of(
+        "column1", Long.class,
+        "column2", BigDecimal.class,
+        "column3", Integer.class,
+        "column4", Double.class,
+        "column5", Boolean.class,
+        "column6", String.class,
+        "column7", Float.class);
+  }
+}
