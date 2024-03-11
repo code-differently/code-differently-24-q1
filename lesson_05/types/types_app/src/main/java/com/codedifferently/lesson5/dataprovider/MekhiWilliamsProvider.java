5ca20c4d1f2eaf7b0e0489874e2fcef59c75ce07
mekhi <mekhi090903@gmail.com>
2024-03-11 13:30:41 +0000

    Feat:mekhiprovider and json file

--- lesson_05/types/types_app/src/main/java/com/codedifferently/lesson5/dataprovider/MekhiWilliamsProvider.java
+++ lesson_05/types/types_app/src/main/java/com/codedifferently/lesson5/dataprovider/MekhiWilliamsProvider.java
@@ -1,19 +1,22 @@
-package main.java.com.codedifferently.lesson5.dataprovider;
+package com.codedifferently.lesson5.dataprovider;
 
 import java.util.Map;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MekhiWilliamsProvider extends DataProvider {
-  @Override
+  public String getProviderName() {
+    return "mekhiwilliams";
+  }
+
   public Map<String, Class> getColumnTypeByName() {
     return Map.of(
-        "column1", Long.class,
-        "column2", String.class,
-        "column3", Double.class,
-        "column4", Double.class,
-        "column5", Boolean.class,
+        "column1", Integer.class,
+        "column2", Boolean.class,
+        "column3", Short.class,
+        "column4", String.class,
+        "column5", Float.class,
         "column6", Long.class,
-        "column7", Integer.class);
+        "column7", Double.class);
   }
 }
