4c36d822034be8431bac5fa118c99e438aa0de81
cibezim <123107253+cibezim@users.noreply.github.com>
2024-03-12 15:28:23 +0000

    fix: adds logic for throw exception when n is less than 0

--- lesson_06/conditionals/conditionals_app/src/main/java/com/codedifferently/lesson6/Lesson6.java
+++ lesson_06/conditionals/conditionals_app/src/main/java/com/codedifferently/lesson6/Lesson6.java
@@ -89,6 +89,9 @@ public class Lesson6 {
    * @return The factorial of n.
    */
   public static int computeFactorial(int n) {
+    if (n < 0) {
+      throw new IllegalArgumentException("Factorial is undefined for negative numbers");
+    }
     int factorial = 1;
     for (int i = 1; i <= n; i++) {
       factorial *= i;
