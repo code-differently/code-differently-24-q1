0965a1401b2080e34432677da7cbb5eb52118ea3
Your Name <warejavyen@gmail.com>
2024-03-13 18:28:00 +0000

    feat: adding answers to problem

--- lesson_06/conditionals/conditionals_app/src/main/java/com/codedifferently/lesson6/Lesson6.java
+++ lesson_06/conditionals/conditionals_app/src/main/java/com/codedifferently/lesson6/Lesson6.java
@@ -20,35 +20,35 @@ public class Lesson6 {
    * @param age The age to check.
    * @return True if the age corresponds to a voting age and false otherwise.
    */
-  public static boolean canVote(int age) { 
+  public static boolean canVote(int age) {
     byte minimumAge = 18;
-    if (age >= minimumAge){
-      return true; 
-    } 
-    else {
+    if (age >= minimumAge) {
+      return true;
+    } else {
       return false;
     }
-  lombok.launch}
-
-
+  }
 
   /**
    * Compares two strings lexographically.
    *
    * @param value1 The first `String` to compare.
    * @param value2 The second `String` to compare.
-   * @rerturn -1 if a is less than b, 1 if a is greater than b, and 0 otherwise.
+   * @return -1 if a is less than b, 1 if a is greater than b, and 0 otherwise.
    */
   public static int compareStrings(String a, String b) {
-    
-    
+
     // The distance will be a number less than 0 if string `a` is lexographically less than `b`, 1
     // if it is greater, and 0 if the strings are equal.
     int distance = Helpers.computeLexographicDistance(a, b);
 
     // TODO(you): Finish this method.
-
-    return 0;
+      if (distance < 0) {
+        return -1;
+      }
+      if (distance > 0 ) {
+        return 1;
+      } else return 0;
   }
 
   /**
@@ -61,19 +61,61 @@ public class Lesson6 {
    * @return The letter grade ("A+", "A", "A-", "B+", etc.).
    */
   public static String convertGpaToLetterGrade(double gpa) {
+    if (gpa >= 4.0) {
+      return "A";
+    } else if (gpa >= 3.99) {
+      return "A-";
+    } else if (gpa >= 3.7) {
+      return "A-";
+    } else if (gpa >= 3.69) {
+      return "B+";
+    } else if (gpa >= 3.3) {
+      return "B+";
+    } else if (gpa >= 3.29) {
+      return "B";
+    } else if (gpa >= 3.0) {
+      return "B";
+    } else if (gpa >= 2.99) {
+      return "B-";
+    } else if (gpa >= 2.7) {
+      return "B-";
+    } else if (gpa >= 2.69) {
+      return "C+";
+    } else if (gpa >= 2.3) {
+      return "C+";
+    } else if (gpa >= 2.29) {
+      return "C";
+    } else if (gpa >= 2.0) {
+      return "C";
+    } else if (gpa >= 1.99) {
+      return "C-";
+    } else if (gpa >= 1.7) {
+      return "C-";
+    } else if (gpa >= 1.69) {
+      return "D+";
+    } else if (gpa >= 1.3) {
+      return "D+";
+    } else if (gpa >= 1.29) {
+      return "D";
+    } else if (gpa >= 1.00) {
+      return "D";
+    } else if (gpa >= 0.99) {
+    }
     return "F";
   }
-
-  /**
+/**
    * Computes the factorial of the given value of `n`.
    *
    * @param n The value for which to compute the factorial.
    * @return The factorial of n.
    */
   public static int computeFactorial(int n) {
-    return 0;
+    int product = 1;
+    for (int i = 1; i <= n; i++) {
+      product *= i;
+    }
+    return product;
   }
-
   /**
    * Adds all of the provided values and returns the sum.
    *
@@ -81,7 +123,11 @@ public class Lesson6 {
    * @return The sum of all the values.
    */
   public static double addNumbers(double[] values) {
-    return 0;
+   double sum = 0;
+   for (int j = 0; j < values.length; j++) {
+    sum = sum + values[j];
+   }
+   return sum; 
   }
 
   /**
@@ -91,7 +137,20 @@ public class Lesson6 {
    * @return An array containing the first `n` fibonacci values.
    */
   public static int[] getFirstNFibonacciNumbers(int n) {
-    return null;
+    if (n < 0) {
+      return null;
+    }
+    int[] fibonacci = new int[n];
+    if (n >= 1) {
+      fibonacci[0] = 1;
+    }
+    if (n >= 2) {
+      fibonacci[1] = 1;
+    }
+    for (int i = 2; i < n; i++) {
+      fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+    }
+    return fibonacci;
   }
 
   /**
@@ -104,19 +163,16 @@ public class Lesson6 {
    * @return The index of the value if found in the array and -1 otherwise.
    */
   public static int binarySearch(int[] values, int start, int end, int value) {
-    if (end < start) {
-      // The range is not valid so just return -1.
-      return -1;
+    while (start <= end) {
+    int pivotIndex = start + (end - start) / 2;
+
+    if (values[pivotIndex] == value) {
+      return pivotIndex;
+    } else if (values[pivotIndex] < value) {
+      start = pivotIndex + 1;
+    } else {
+      end = pivotIndex -1;
     }
-
-    int pivotIndex = (start + end) / 2; // The index in the middle of the array.
-
-    // TODO(you): Finish implementing this algorithm
-
-    // If values[pivotIndex] is equal to value then return `pivotIndex`.
-    // Else if values[pivotIndex] is greater than the value, then
-    // call `binarySearch(values, start, pivotIndex - 1, value)` and return its value;
-    // Else call `binarySearch(values, pivotIndex + 1, end, value)` and return its value.
     return -1;
   }
 }
