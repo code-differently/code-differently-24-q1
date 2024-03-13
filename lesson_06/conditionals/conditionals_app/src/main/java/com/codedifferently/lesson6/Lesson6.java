0cda65d36153d4043ca0d45864b58a8454642103
Nelshulla <Jadanicole0@gmail.com>
2024-03-13 15:22:43 +0000

    feat: added Jada's completed lesson 06

--- lesson_06/conditionals/conditionals_app/src/main/java/com/codedifferently/lesson6/Lesson6.java
+++ lesson_06/conditionals/conditionals_app/src/main/java/com/codedifferently/lesson6/Lesson6.java
@@ -21,7 +21,13 @@ public class Lesson6 {
    * @return True if the age corresponds to a voting age and false otherwise.
    */
   public static boolean canVote(int age) {
-    return false;
+    if (age >= 18) {
+      return true;
+    } else if (age < 18) {
+      return false;
+    } else {
+      return false;
+    }
   }
 
   /**
@@ -35,10 +41,13 @@ public class Lesson6 {
     // The distance will be a number less than 0 if string `a` is lexographically less than `b`, 1
     // if it is greater, and 0 if the strings are equal.
     int distance = Helpers.computeLexographicDistance(a, b);
-
-    // TODO(you): Finish this method.
-
-    return 0;
+    if (distance < 0) {
+      return -1;
+    } else if (distance > 0) {
+      return 1;
+    } else {
+      return 0;
+    }
   }
 
   /**
@@ -51,17 +60,39 @@ public class Lesson6 {
    * @return The letter grade ("A+", "A", "A-", "B+", etc.).
    */
   public static String convertGpaToLetterGrade(double gpa) {
-    return "F";
+    if (gpa >= 4.0) {
+      return "A";
+    } else if (gpa >= 3.7) {
+      return "A-";
+    } else if (gpa >= 3.3) {
+      return "B+";
+    } else if (gpa >= 3.0) {
+      return "B";
+    } else if (gpa >= 2.7) {
+      return "B-";
+    } else if (gpa >= 2.3) {
+      return "C+";
+    } else if (gpa >= 2.0) {
+      return "C";
+    } else if (gpa >= 1.7) {
+      return "C-";
+    } else if (gpa >= 1.3) {
+      return "D+";
+    } else if (gpa >= 1.0) {
+      return "D";
+    } else if (gpa >= 0.7) {
+      return "F";
+    } else {
+      return "F";
+    }
   }
 
-  /**
-   * Computes the factorial of the given value of `n`.
-   *
-   * @param n The value for which to compute the factorial.
-   * @return The factorial of n.
-   */
   public static int computeFactorial(int n) {
-    return 0;
+    if (n == 0 || n == 1) {
+      return 1;
+    } else {
+      return n * computeFactorial(n - 1);
+    }
   }
 
   /**
@@ -71,7 +102,12 @@ public class Lesson6 {
    * @return The sum of all the values.
    */
   public static double addNumbers(double[] values) {
-    return 0;
+    double sum = 0.0;
+    for (double value : values) {
+      // The value represnt each individual number in the list of numbers
+      sum += value;
+    }
+    return sum;
   }
 
   /**
@@ -81,7 +117,17 @@ public class Lesson6 {
    * @return An array containing the first `n` fibonacci values.
    */
   public static int[] getFirstNFibonacciNumbers(int n) {
-    return null;
+    int[] fibonacci = new int[n];
+    if (n >= 1) {
+      fibonacci[0] = 1;
+    }
+    if (n >= 2) {
+      fibonacci[1] = 1;
+    }
+    for (int i = 2; i < n; i++) {
+      fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+    }
+    return fibonacci;
   }
 
   /**
@@ -100,13 +146,10 @@ public class Lesson6 {
     }
 
     int pivotIndex = (start + end) / 2; // The index in the middle of the array.
-
-    // TODO(you): Finish implementing this algorithm
-
-    // If values[pivotIndex] is equal to value then return `pivotIndex`.
-    // Else if values[pivotIndex] is greater than the value, then
-    // call `binarySearch(values, start, pivotIndex - 1, value)` and return its value;
-    // Else call `binarySearch(values, pivotIndex + 1, end, value)` and return its value.
-    return -1;
+    if (values[pivotIndex] == value) {
+      return pivotIndex;
+    } else if (values[pivotIndex] > value) {
+      return binarySearch(values, start, pivotIndex - 1, value);
+    } else return binarySearch(values, pivotIndex + 1, end, value);
   }
 }
