236a50378d6898ed2af611e73e69335f1ce2416a
kjknows <kmase1022@gmail.com>
2024-03-13 14:37:20 +0000

    feat(data-provider): Add KevinmasonProvider service
    Add KevinmasonProvider class which extends DataProvider and implements methods to retrieve provider name and column types by name.

--- lesson_05/types/types_app/src/main/java/com/codedifferently/lesson5/dataprovider/KevinmasonProvider.java
+++ lesson_05/types/types_app/src/main/java/com/codedifferently/lesson5/dataprovider/KevinmasonProvider.java
@@ -1,7 +1,9 @@
 package com.codedifferently.lesson5.dataprovider;
 
 import java.util.Map;
-@service
+import org.springframework.stereotype.Service;
+
+@Service
 public class KevinmasonProvider extends DataProvider {
   public String getProviderName() {
     return "Kevinmason";
@@ -11,10 +13,10 @@ public class KevinmasonProvider extends DataProvider {
     return Map.of(
         "column1", String.class,
         "column2", Double.class,
-        "column3", Integer.class,
+        "column3", Short.class,
         "column4", Float.class,
         "column5", Long.class,
         "column6", Boolean.class,
-        "column7", Short.class);
+        "column7", Integer.class);
   }
 }
