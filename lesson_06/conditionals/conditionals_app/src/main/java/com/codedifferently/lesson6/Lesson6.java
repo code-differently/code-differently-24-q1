4228f44f49ef53af67715b92316843ef2d58af82
Aaron2278 <aaronsantiago542@gmail.com>
2024-03-13 16:14:11 +0000

    Feat: Aaron lesson 6

--- lesson_06/conditionals/conditionals_app/src/main/java/com/codedifferently/lesson6/Lesson6.java
+++ lesson_06/conditionals/conditionals_app/src/main/java/com/codedifferently/lesson6/Lesson6.java
@@ -1,6 +1,6 @@
 package com.codedifferently.lesson6;
 
-import com.codedifferently.lesson6.util.Helpers;
+import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
@@ -10,103 +10,74 @@ import org.springframework.context.annotation.Configuration;
 public class Lesson6 {
 
   public static void main(String[] args) {
-    var application = new SpringApplication(Lesson6.class);
-    application.run(args);
+    SpringApplication.run(Lesson6.class, args);
   }
 
-  /**
-   * Returns true if the provided age meets the minimum US voting age and false otherwise.
-   *
-   * @param age The age to check.
-   * @return True if the age corresponds to a voting age and false otherwise.
-   */
   public static boolean canVote(int age) {
-    return false;
+    return age >= 18;
+  }
+
+  public static int compareStrings(String value1, String value2) {
+    return value1.compareTo(value2);
   }
 
   /**
-   * Compares two strings lexographically.
-   *
-   * @param value1 The first `String` to compare.
-   * @param value2 The second `String` to compare.
-   * @rerturn -1 if a is less than b, 1 if a is greater than b, and 0 otherwise.
-   */
-  public static int compareStrings(String a, String b) {
-    // The distance will be a number less than 0 if string `a` is lexographically less than `b`, 1
-    // if it is greater, and 0 if the strings are equal.
-    int distance = Helpers.computeLexographicDistance(a, b);
-
-    // TODO(you): Finish this method.
-
-    return 0;
-  }
-
-  /**
-   * Converts a GPA on the 4.0 scale to the corresponding letter grade using the college board
-   * scale. See
-   * https://bigfuture.collegeboard.org/plan-for-college/college-basics/how-to-convert-gpa-4.0-scale
-   * for details.
+   * Converts a GPA on the 4.0 scale to the corresponding letter grade.
    *
    * @param gpa The GPA value.
-   * @return The letter grade ("A+", "A", "A-", "B+", etc.).
+   * @return The letter grade ("A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", or
+   *     "F").
    */
   public static String convertGpaToLetterGrade(double gpa) {
+    if (gpa >= 4.0) return "A";
+    if (gpa >= 3.7) return "A-";
+    if (gpa >= 3.3) return "B+";
+    if (gpa >= 3.0) return "B";
+    if (gpa >= 2.7) return "B-";
+    if (gpa >= 2.3) return "C+";
+    if (gpa >= 2.0) return "C";
+    if (gpa >= 1.7) return "C-";
+    if (gpa >= 1.3) return "D+";
+    if (gpa >= 1.0) return "D";
     return "F";
   }
 
-  /**
-   * Computes the factorial of the given value of `n`.
-   *
-   * @param n The value for which to compute the factorial.
-   * @return The factorial of n.
-   */
   public static int computeFactorial(int n) {
-    return 0;
+    return n == 0 ? 1 : n * computeFactorial(n - 1);
   }
 
-  /**
-   * Adds all of the provided values and returns the sum.
-   *
-   * @param values The values to sum.
-   * @return The sum of all the values.
-   */
   public static double addNumbers(double[] values) {
-    return 0;
+    return Arrays.stream(values).sum();
   }
 
-  /**
-   * Returns an array of the first `n` fibonacci numbers starting from 1.
-   *
-   * @param n The first `n` of fibonacci values to compute.
-   * @return An array containing the first `n` fibonacci values.
-   */
   public static int[] getFirstNFibonacciNumbers(int n) {
-    return null;
+    if (n < 1) return new int[0]; // Handle cases where n < 1
+    int[] fib = new int[n];
+    fib[0] = 1;
+    if (n > 1) {
+      fib[1] = 1;
+      for (int i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+      }
+    }
+    return fib;
   }
 
-  /**
-   * Finds a value in an array of values.
-   *
-   * @param values The values to search.
-   * @param The left most index to search.
-   * @param The right most index to search.
-   * @param value The value to look for.
-   * @return The index of the value if found in the array and -1 otherwise.
-   */
+  public static int binarySearch(int[] values, int value) {
+    return binarySearch(values, 0, values.length - 1, value);
+  }
+
   public static int binarySearch(int[] values, int start, int end, int value) {
-    if (end < start) {
-      // The range is not valid so just return -1.
-      return -1;
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+      if (values[mid] == value) {
+        return mid;
+      } else if (values[mid] < value) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
     }
-
-    int pivotIndex = (start + end) / 2; // The index in the middle of the array.
-
-    // TODO(you): Finish implementing this algorithm
-
-    // If values[pivotIndex] is equal to value then return `pivotIndex`.
-    // Else if values[pivotIndex] is greater than the value, then
-    // call `binarySearch(values, start, pivotIndex - 1, value)` and return its value;
-    // Else call `binarySearch(values, pivotIndex + 1, end, value)` and return its value.
     return -1;
   }
 }
