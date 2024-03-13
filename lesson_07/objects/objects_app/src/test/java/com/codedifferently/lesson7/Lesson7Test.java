adfd87d89f8961c702328f1e32e1a11ecc11e27a
Anthony D. Mays <anthonydmays@users.noreply.github.com>
2024-03-13 11:30:26 -0700

    feat: adds lesson 07 content and assignment details (#201)
    * feat: adding empty project template
    
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>
    
    * docs: updating assignment details
    
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>
    
    * docs: adds assignment requirements
    
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>
    
    * docs: updates test requirements
    
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>
    
    * chore: adds build checks
    
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>
    
    * chore: updating build script
    
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>
    
    ---------
    
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>

---
+++ lesson_07/objects/objects_app/src/test/java/com/codedifferently/lesson7/Lesson7Test.java
@@ -0,0 +1,13 @@
+package com.codedifferently.lesson7;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class Lesson7Test {
+
+  @Test
+  void testCanVote() {
+    assertThat(new Lesson7()).isNotNull();
+  }
+}
