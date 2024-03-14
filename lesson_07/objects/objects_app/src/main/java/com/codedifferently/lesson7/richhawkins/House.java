3ce2aa3848b8294e4ca8ea7057cb162c3c98ce84
RichHawkins <RichHawkins1986@gmail.com>
2024-03-14 01:37:05 +0000

    feat: adds new class and 5 member variables

---
+++ lesson_07/objects/objects_app/src/main/java/com/codedifferently/lesson7/richhawkins/House.java
@@ -0,0 +1,20 @@
+package main.java.com.codedifferently.lesson7.richhawkins;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class House {
+    //     The class must have at least 5 member variables of at least 3 different types.
+    ArrayList<String> streetName = new ArrayList<>();
+    ArrayList<Integer> streetNum = new ArrayList<>();
+    short yearBuilt;
+    boolean hasGarage;
+    short numberOfBedrooms;
+    // You must also incorporate an enum type as well.
+    // The class must have at least one constructor.
+    // The class must have at least 3 member functions.
+    // One of your functions must make use of a conditional expression.
+    // One of your functions must make use of a loop.
+    // You must use at least one custom exception.
+}
