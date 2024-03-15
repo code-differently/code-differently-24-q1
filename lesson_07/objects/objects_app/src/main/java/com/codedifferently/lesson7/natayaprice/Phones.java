42ef5b47d31d16d05b2b1ff0c63e63761b53048b
Nataya Price <nprice@thecodesociety.onmicrosoft.com>
2024-03-15 17:04:15 +0000

    feat: Completed java file & test file

---
+++ lesson_07/objects/objects_app/src/main/java/com/codedifferently/lesson7/natayaprice/Phones.java
@@ -0,0 +1,85 @@
+package com.codedifferently.lesson7.natayaprice;
+
+
+
+
+public class Phones {
+    
+    public enum Color {
+        BLUE,
+        GREEN,
+        PINK,
+        BLACK,
+        WHITE,
+        YELLOW; 
+     } 
+ 
+ private String color;
+ private String serviceProvider;
+ private int storageCapacity;
+ private double price;
+ private String brand;
+
+
+
+ public Phones(String color, double price, String brand, String serviceProvider, int storageCapacity)  {
+    this.color = color;
+    this.price = price;
+    this.brand = brand;
+    this.serviceProvider = serviceProvider;
+    this.storageCapacity = storageCapacity;
+
+} 
+
+public void listColor() {
+    for(int i = 0; i < Color.values().length; i++) {
+        Color color = Color.values()[i];
+        System.out.println(color);
+    }
+} 
+
+public Object getColor() {
+    return color;
+}
+
+
+public static void comparePrice (int price1, int price2) {
+    int difference = price1 - price2;
+    System.out.println("The difference between " + price1 + " && " + price2 + " is: " + difference);
+} 
+
+public double getPrice () {
+    return price;
+}
+
+public void setPrice(double price) throws InvalidPriceException {
+    if (price < 0) {
+      throw new InvalidPriceException("Price cannot be negative.");
+    }
+    this.price = price;
+  } 
+
+  public class InvalidPriceException extends Exception {
+    public InvalidPriceException(String message) {
+      super(message);
+    }
+  }
+
+public int getStoragecapacity() {
+    return storageCapacity;
+}
+
+public String getServiceprovider() {
+    return serviceProvider;
+}
+ 
+   public String getBrand() {
+    return brand;
+} 
+
+ }
+
+
+
+
+
---
+++ lesson_07/objects/objects_app/src/test/java/com/codedifferently/lesson7/natayaprice/PhonesTest.java
@@ -0,0 +1,83 @@
+package com.codedifferently.lesson7.natayaprice; 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+
+
+import org.junit.jupiter.api.Test;
+
+import com.codedifferently.lesson7.natayaprice.Phones.InvalidPriceException;
+
+public class PhonesTest {
+
+    @Test
+    public void testConstructor() {
+        Phones phone = new Phones("BLUE", 500.0, "Samsung", "Verizon", 128);
+        assertNotNull(phone);
+        assertEquals("BLUE", phone.getColor());
+        assertEquals(500.0, phone.getPrice(), 0.01);
+        assertEquals("Samsung", phone.getBrand());
+        assertEquals("Verizon", phone.getServiceprovider());
+        assertEquals(128, phone.getStoragecapacity());
+    }
+
+ @Test
+    public void testGetColor() {
+        Phones phone = new Phones("BLUE", 500.0, "Samsung", "Verizon", 128);
+        assertEquals("BLUE", phone.getColor());
+    }
+
+
+    @Test
+    public void testGetPrice() {
+        Phones phone = new Phones("GREEN", 600.0, "Apple", "AT&T", 256);
+        assertEquals(600.0, phone.getPrice(), 0.01);
+    }
+
+    @Test
+    public void testSetNegativePrice() throws Phones.InvalidPriceException {
+        Phones phone = new Phones("PINK", 700.0, "Google", "T-Mobile", 64);
+        Exception exception =
+        assert throws(
+            InvalidPriceException.class, 
+            () -> {
+            phone.setPrice(-100.0)
+        });
+    }
+
+    @Test
+    public void testGetStorageCapacity() {
+        Phones phone = new Phones("BLACK", 800.0, "OnePlus", "Sprint", 512);
+        assertEquals(512, phone.getStoragecapacity());
+    }
+
+    @Test
+    public void testGetServiceProvider() {
+        Phones phone = new Phones("WHITE", 900.0, "Huawei", "AT&T", 256);
+        assertEquals("AT&T", phone.getServiceprovider());
+    }
+
+    @Test
+    public void testGetBrand() {
+        Phones phone = new Phones("YELLOW", 1000.0, "Motorola", "Verizon", 128);
+        assertEquals("Motorola", phone.getBrand());
+    }
+
+    @Test
+    public void testComparePrice() {
+        Phones.comparePrice(500, 600);
+        // Output should be "The difference between 500 && 600 is: -100"
+    }
+
+    @Test
+    public void testListColor() {
+        Phones phone = new Phones("BLUE", 500.0, "Samsung", "Verizon", 128);
+        phone.listColor();
+        // This test will print out all colors in the Color enum
+    }
+}
+
+    
+
