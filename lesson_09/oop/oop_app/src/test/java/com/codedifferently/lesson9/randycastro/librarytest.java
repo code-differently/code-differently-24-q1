f623c6ab3dbcd6e3a5dc9f92a28c886902923024
RandyCastr0 <randyyc15@gmail.com>
2024-03-19 18:28:27 +0000

    feat: updates to test 1

--- lesson_09/oop/oop_app/src/test/java/com/codedifferently/lesson9/randycastro/librarytest.java
+++ lesson_09/oop/oop_app/src/test/java/com/codedifferently/lesson9/randycastro/librarytest.java
@@ -1,53 +1,29 @@
 package com.codedifferently.lesson9.randycastro;
 
+import com.codedifferently.lesson9.randycastro.library.Book;
 import org.junit.jupiter.api.Test;
 
-public class librarytest{
+public class librarytest {
 
-public class BookTest{
+  public class BookTest {
 
-@Test
- // testing Book constructor 
- // Arrange
- public void testBookConstructor() {
-    String title = "dd";
-    String isbn = "f32d";
-    String author = "mas";
-    int page = 33;
-    boolean checkedOut = false;
-    //Act
-    Book book == new Book (title, isbn, author, page, checkedOut);
-
-
- }
-
-
-
-
-
+    @Test
+    // testing Book constructor
+    // Arrange
+    public void testBookConstructor() {
+      String title = "dd";
+      String isbn = "f32d";
+      String author = "mas";
+      int page = 33;
+      boolean checkedOut = false;
+      // Act
+      Book book = new Book("efea", "f32d", "mas", 33, false);
+      // Assert
+      assesrtsEquals(title, book.getTitle());
+      assertsEquals(isbn, book.getIsbn());
+      assertsEquals(author, book.getAuthor());
+      assertsEquals (page, book.getPage());
+      assesrtsEquals(checkedOut, book.getCheckedout());
+    }
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-        
-
-
-
-
-
-
