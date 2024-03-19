cdcd521a295501c17f44a6f83129ce9cbf3ef508
Anthony D. Mays <anthony@morganlatimer.com>
2024-03-19 08:54:08 -0700

    fix: corrected code and tests files
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>

--- lesson_07/objects/objects_app/src/main/java/com/codedifferently/lesson7/kyvonthompson/Frog.java
+++ lesson_07/objects/objects_app/src/main/java/com/codedifferently/lesson7/kyvonthompson/Frog.java
@@ -1,27 +1,25 @@
 package com.codedifferently.lesson7.kyvonthompson;
 
+public class Frog {
+  // Frog Attributes
 
+  private String color;
+  private int age; // Age in years
+  private int size; // Size in inches
+  private double weight; // Weigh in pounds
+  private String breed;
 
-public class frog {
- //Frog Attributes
-
- private String color;
- private int age; //Age in years
- private int size; //Size in inches
- private double weight; //Weigh in pounds
- private String breed;
-
- //Constructer to create brand new frogger
- public frog(String color, int age, int size, double weight, String breed) {
+  // Constructer to create brand new frogger
+  public Frog(String color, int age, int size, double weight, String breed) {
 
     this.color = color;
     this.age = age;
     this.size = size;
     this.weight = weight;
     this.breed = breed;
+  }
 
- }
- public enum frogColor {
+  public enum frogColor {
     GREEN,
     BLUE,
     RED,
@@ -29,72 +27,76 @@ public class frog {
     PINK,
     WHITE,
     BROWN,
- }
+  }
 
- public enum frogType {
+  public enum frogType {
     TREE_FROG,
     BULLFROG,
     POISON_DART_FROG,
     LEOPARD_FROG,
     COMMON_FROG,
     GLASS_FROG
-}
- // Behaviors of a Frog
+  }
 
-    // The frog jumps
-    public void jump() {
-        System.out.println("The " + color + " frog jumps!");
-    }
+  // Behaviors of a Frog
 
-    // The frog croaks
-    public void croak() {
-        System.out.println("The " + color + " frog says: Croak!");
-    }
+  // The frog jumps
+  public String jump() {
+    return "The " + color + " frog jumps!";
+  }
 
-    // Getter for color
-    public String getColor() {
-        return color;
-    }
+  // The frog croaks
+  public void croak() {
+    System.out.println("The " + color + " frog says: Croak!");
+  }
 
-    // Setter for color
-    public void setColor(String color) {
-        this.color = color;
-    }
+  // Getter for color
+  public String getColor() {
+    return color;
+  }
 
-    // Getter for size
-    public int getSize() {
-        return size;
-    }
+  // Setter for color
+  public void setColor(String color) {
+    this.color = color;
+  }
 
-    // Setter for size
-    public void setSize(int size) {
-        this.size = size;
-    }
+  // Getter for size
+  public int getSize() {
+    return size;
+  }
 
-    // Getter for age
-    public int getAge() {
-        return age;
-    }
+  // Setter for size
+  public void setSize(int size) {
+    this.size = size;
+  }
 
-    // Setter for age
-    public void setAge(int age) {
-        this.age = age;
-    }
-    // Getter for weight
-    public double getWeight() {
-        return weight;
-    }
-    // Setter for weight
-    public void setWeight(double weight) {
-        this.weight = weight; 
-    }
-    // Getter for breed
-    public String getBreed(){
-        return breed;
-    }
-    // Setter for breed
-    public void setBreed(String breed){
-        this.breed = breed;
-    }
-    
+  // Getter for age
+  public int getAge() {
+    return age;
+  }
+
+  // Setter for age
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  // Getter for weight
+  public double getWeight() {
+    return weight;
+  }
+
+  // Setter for weight
+  public void setWeight(double weight) {
+    this.weight = weight;
+  }
+
+  // Getter for breed
+  public String getBreed() {
+    return breed;
+  }
+
+  // Setter for breed
+  public void setBreed(String breed) {
+    this.breed = breed;
+  }
 }
--- lesson_07/objects/objects_app/src/test/java/com/codedifferently/lesson7/kyvonthompson/FrogTest.java
+++ lesson_07/objects/objects_app/src/test/java/com/codedifferently/lesson7/kyvonthompson/FrogTest.java
@@ -1,17 +1,10 @@
 package com.codedifferently.lesson7.kyvonthompson;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-
-public class Frog {
-
-  public Frog(String string, int i, int j, double d, String string2) {
-    
-  
+public class FrogTest {
 
   @Test
   // Frog actions
@@ -20,21 +13,10 @@ public class Frog {
     // Creating a new Frog instance
     Frog frog = new Frog("green", 5, 2, 8.2, "bulletFrog");
     // Getting frog details
-    assertEquals("The green frog jumps!", frog.jumps());
+    assertEquals("The green frog jumps!", frog.jump());
   }
 
-  private Frogtest jumps() {
+  private void jumps() {
     throw new UnsupportedOperationException("Unimplemented method 'jumps'");
   }
-  }
-  // System.out.println(
-  //     "frog details: Color - "
-  //         + frog.getColor()
-  //         + ", Size - "
-  //         + frog.getSize()
-  //         + ", Age - "
-  //         + frog.getAge()
-  //         + ",Weight - "
-  //         + frog.getWeight()
-  //         + ",Weight"
-  //         + frog.getBreed());
+}
