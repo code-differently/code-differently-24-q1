3ca8a2a4e0df670790cdaef7163b4d60d646c67b
RandyCastr0 <160950862+RandyCastr0@users.noreply.github.com>
2024-03-18 21:10:56 -0400

    feat: adds Randy's Lesson 07 response (#220)
    * feat: added answer for test 1
    
    * feat: completed question 2
    
    * feat: completed question 3
    
    * chore: completed 2 more questions.
    
    * chore: completed fibonacci question.
    
    * chore: completed all questions
    
    * feat: added "weather file" and "weathertest" file
    
    * feat: added enum file
    
    * feat: added functions.
    
    * chore: fixed format
    
    * chore: fixed methods
    
    * chore: moved files around.
    
    * chore: updates with format
    
    * feat: added a test
    
    * feat: added exception
    
    * chore: fixed constructor
    
    * chore: clean up format
    
    * feat: added testConstructor
    
    * feat: added testCheckWeatherConditionSunny, testCheckWeatherConditionRainy
    
    * feat: added test testPrintTemperatures
    
    * chore: updates testsetandgetsunrise
    
    * Restore Lesson6.java
    
    ---------
    
    Co-authored-by: Anthony D. Mays <anthonydmays@users.noreply.github.com>

---
+++ lesson_07/objects/objects_app/src/main/java/com/codedifferently/randycastro/Weather.java
@@ -0,0 +1,205 @@
+package com.codedifferently.randycastro;
+
+import java.lang.reflect.Array;
+
+import com.codedifferently.randycastro.Weather.WeatherCondition;
+
+public class Weather {
+   //enum
+public enum WeatherCondition {
+    SUNNY, RAINY;
+}
+private int temp;
+private String windDirection;
+private WeatherCondition weatherCondition;
+private int sunrise;
+private int sunset;
+// Constructor
+public Weather(int temp, String windDirection, WeatherCondition weatherCondition, int sunrise, int sunset) {
+     
+  this.temp = temp;
+  this.windDirection = windDirection;
+  this.weatherCondition = weatherCondition;
+  this.sunrise = sunrise;
+  this.sunset = sunset;
+}
+  
+  // functions//
+  // created getters and setters 
+
+  public Weather() {
+  //TODO Auto-generated constructor stub
+}
+
+  public int getTemp() {
+    return temp;
+  }
+
+  public void setTemp(int temp) {
+    this.temp = temp;
+  }
+
+  public String getWindDirection() {
+    return windDirection;
+  }
+
+  public void setWindDirection(String windDirection) {
+    this.windDirection = windDirection;
+  }
+
+  public WeatherCondition getWeatherCondition() {
+    return weatherCondition;
+  }
+
+  public int getSunrise() {
+    return sunrise;
+  }
+
+  public void setSunrise( int i) {
+    this.sunrise = i;
+  }
+
+  public int getSunset() {
+    return sunset;
+  }
+
+ 
+ //public class TempOutOfBoundsException extends Exception {
+    //public TempOutOfBoundsException(String message) {
+       // super(message);
+     
+  
+
+
+// if loop, If sunny it will print out "It will be a sunny day!". Else, it will print out "It will be a rainy day!".
+
+
+  
+//Array
+
+// Constructor for Array 
+ 
+public String checkWeatherCondition() {
+  if (weatherCondition == WeatherCondition.SUNNY) {
+      return "It will be a sunny day!";
+  } else {
+      return "It will be a rainy day!";
+  }
+}
+
+
+
+
+public int printTemperatures() {
+  int[] temperatures = {10,20,30,40,50,60,70,80,90,100};
+  int count = 0;
+  for(int i = 0; i < temperatures.length; i++) {
+      try {
+          // Print temperature information
+          System.out.println("Temperature " + (i + 1) + ": " + temperatures[i]);
+      } catch (ArrayIndexOutOfBoundsException e) {
+          // Handle the exception (if index is out of bounds)
+          System.out.println("Error: Temperature is out of bounds[] " + i);
+}
+  }
+  return count;
+}
+
+public void setWeatherCondition(WeatherCondition sunny) {
+ 
+}
+}
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for loop
+
+
+
+
+/*                                     5 Member Variables
+1 Temp                data type: int
+2 Wind direction      data type: String
+3 Weather condition   data type: Boolean
+4 Humidity            data type: int
+5 Sunrise/ Sunset      data type: int
+
+                                        3 Different Types
+1 int
+2 boolean
+3 double
+
+                                            Enum
+
+public enum Weathercondition {
+    Sunny
+    Rainy
+    Snow
+}
+                                            Constructor
+
+public class weather {
+    int temp
+    string condition
+
+}
+(int temp, wind direction, weather condition, humidity, sunrise/ sunset)
+
+Constructing weather by including these variables
+
+
+
+                                            3 Member Functions
+1
+2
+3
+
+                                            1 Conditional expressions (if)
+1 if time == 7:13am - 7:33am
+print sunrise
+else if time == 7:09pm - 7:39pm
+print sunset
+else
+print
+
+
+                                            Loop (for, while)
+
+        for()
+
+
+
+                                            1 Custom Exception
+1
+
+
+
+
+
+                                       Collection (array)
+
+
+*/
