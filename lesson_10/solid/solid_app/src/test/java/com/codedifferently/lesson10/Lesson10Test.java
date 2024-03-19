c96362dfd2b62992461b30f42550fce984a018b1
Anthony D. Mays <anthony@morganlatimer.com>
2024-03-18 19:37:38 -0700

    chore: adds boilerplate
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>

---
+++ lesson_10/solid/solid_app/src/test/java/com/codedifferently/lesson10/Lesson10Test.java
@@ -0,0 +1,13 @@
+package com.codedifferently.lesson10;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class Lesson10Test {
+
+  @Test
+  void testInstantiate() {
+    assertThat(new Lesson10()).isNotNull();
+  }
+}
