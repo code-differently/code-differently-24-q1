ff8a1b804e91faca2166382f0077c8a50199bc5f
Mohamed <moibrahi7@gmail.com>
2024-03-20 12:43:02 +0000

    Chore: removed not need import

--- lesson_09/oop/oop_app/src/test/java/com/codedifferently/lesson9/mohamedibrahim/BookTest.java
+++ lesson_09/oop/oop_app/src/test/java/com/codedifferently/lesson9/mohamedibrahim/BookTest.java
@@ -2,7 +2,6 @@ package com.codedifferently.lesson9.mohamedibrahim;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
