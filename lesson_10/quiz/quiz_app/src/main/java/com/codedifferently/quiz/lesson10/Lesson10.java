a91ee53ef2cb117a29e55e26dca94dcd8e602f9f
Anthony D. Mays <anthony@morganlatimer.com>
2024-03-19 19:55:30 -0700

    feat: adds support for loading quiz questions from config

--- lesson_10/quiz/quiz_app/src/main/java/com/codedifferently/quiz/lesson10/Lesson10.java
+++ lesson_10/quiz/quiz_app/src/main/java/com/codedifferently/quiz/lesson10/Lesson10.java
@@ -1,15 +1,24 @@
 package com.codedifferently.quiz.lesson10;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import com.codedifferently.instructional.quiz.QuizConfig;
+import org.springframework.boot.CommandLineRunner;
 
 @Configuration
 @SpringBootApplication(scanBasePackages = "com.codedifferently")
-public class Lesson10 {
+public class Lesson10 implements CommandLineRunner {
+  @Autowired
+  private QuizConfig quizConfig;
 
   public static void main(String[] args) {
     var application = new SpringApplication(Lesson10.class);
     application.run(args);
+  }
+
+  public void run(String... args) {
+      System.out.println("choices are" + quizConfig.getQuestions("default").get(0));
   }
 }
--- lib/java/codedifferently-instructional/instructional-lib/src/main/java/com/codedifferently/instructional/quiz/QuizConfig.java
+++ lib/java/codedifferently-instructional/instructional-lib/src/main/java/com/codedifferently/instructional/quiz/QuizConfig.java
@@ -1,13 +1,16 @@
 package com.codedifferently.instructional.quiz;
 
-import at.favre.lib.crypto.bcrypt.BCrypt;
-import at.favre.lib.crypto.bcrypt.BCrypt.Verifyer;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
 import org.springframework.stereotype.Component;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import at.favre.lib.crypto.bcrypt.BCrypt.Verifyer;
 
 /** Loads quiz answers from the config file and uses bcrypt to check provided answers. */
 @Component
@@ -15,11 +18,19 @@ import org.springframework.stereotype.Component;
 public class QuizConfig {
 
   private Map<String, List<String>> answersByProvider;
-
+  private Map<String, List<QuestionConfig>> questionsByProvider;
   private Verifyer verifyer = BCrypt.verifyer();
 
   public void setAnswers(Map<String, List<String>> answersByProvider) {
     this.answersByProvider = answersByProvider;
+  }
+
+  public void setQuestions(Map<String, List<QuestionConfig>> questionsByProvider) {
+      this.questionsByProvider = questionsByProvider;
+  }
+
+  public List<QuestionConfig> getQuestions(String provider) {
+      return this.questionsByProvider.get(provider);
   }
 
   public boolean checkAnswer(String provider, int questionNumber, String actualAnswer) {
@@ -36,4 +47,6 @@ public class QuizConfig {
     }
     return answersByProvider.get(provider).size();
   }
+
+  record QuestionConfig(String prompt, Map<AnswerChoice, String> choices) {}
 }
