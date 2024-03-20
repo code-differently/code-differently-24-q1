98169558217147aa72543d0410a8c7af3c1323b6
KJknows <kmase1022@gmail.com>
2024-03-20 15:39:06 +0000

    fix: added Library file

--- lesson_09/oop/oop_app/src/main/java/com/codedifferently/lesson9/kevinmason/Library.java
+++ lesson_09/oop/oop_app/src/main/java/com/codedifferently/lesson9/kevinmason/Library.java
@@ -4,42 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
-    private List<Book> books;
-    private List<Patron> patrons;
+  private List<Book> books;
+  private List<Patron> patrons;
 
-    public Library() {
-        this.books = new ArrayList<>();
-        this.patrons = new ArrayList<>();
-    }
+  public Library() {
+    this.books = new ArrayList<>();
+    this.patrons = new ArrayList<>();
+  }
 
-    public void addBook(Book book) {
-        books.add(book);
-    }
+  public void addBook(Book book) {
+    books.add(book);
+  }
 
-    public void removeBook(Book book) {
-        books.remove(book);
-    }
+  public void removeBook(Book book) {
+    books.remove(book);
+  }
 
-    public void registerPatron(Patron patron) {
-        patrons.add(patron);
-    }
+  public void registerPatron(Patron patron) {
+    patrons.add(patron);
+  }
 
-    public void checkOutBook(Book book, Patron patron) {
-        if (!book.isCheckedOut()) {
-            book.setCheckedOut(true);
-            patron.addCheckedOutBook(book);
-        }
+  public void checkOutBook(Book book, Patron patron) {
+    if (!book.isCheckedOut()) {
+      book.setCheckedOut(true);
+      patron.addCheckedOutBook(book);
     }
+  }
 
-    public void returnBook(Book book, Patron patron) {
-        book.setCheckedOut(false);
-        patron.removeCheckedOutBook(book);
-    }
+  public void returnBook(Book book, Patron patron) {
+    book.setCheckedOut(false);
+    patron.removeCheckedOutBook(book);
+  }
 
-    public List<Book> getBooks() {
-        return books;
-    }
+  public List<Book> getBooks() {
+    return books;
+  }
 }
-
-
-
--- lesson_09/oop/oop_app/src/test/java/com/codedifferently/lesson9/kevinmason/LibraryTest.java
+++ lesson_09/oop/oop_app/src/test/java/com/codedifferently/lesson9/kevinmason/LibraryTest.java
@@ -1,32 +1,29 @@
 package com.codedifferently.lesson9.kevinmason;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
+import org.junit.jupiter.api.Test;
 
 public class LibraryTest {
 
-    @Test
-    public void testAddBook() {
-        // Create a library
-        Library library = new Library();
+  @Test
+  public void testAddBook() {
+    // Create a library
+    Library library = new Library();
 
-        // Create a book
-        Book book = new Book("Test Book", "ISBN123", "Test Author", 200);
+    // Create a book
+    Book book = new Book("Test Book", "ISBN123", "Test Author", 200);
 
-        // Add the book to the library
-        library.addBook(book);
+    // Add the book to the library
+    library.addBook(book);
 
-        // Retrieve the list of books from the library
-        List<Book> books = library.getBooks();
+    // Retrieve the list of books from the library
+    List<Book> books = library.getBooks();
 
-        // Verify that the book was added to the library
-        assertTrue(books.contains(book));
-    }
+    // Verify that the book was added to the library
+    assertTrue(books.contains(book));
+  }
 
-    // You can add more test methods for other functionalities here...
+  // You can add more test methods for other functionalities here...
 }
-
-
-
