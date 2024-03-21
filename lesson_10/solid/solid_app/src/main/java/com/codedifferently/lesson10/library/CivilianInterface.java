b99065e09836184801f829308f4c42f44be568fd
RandyCastr0 <randyyc15@gmail.com>
2024-03-21 20:33:08 +0000

    feat: created interface and librarian class

---
+++ lesson_10/solid/solid_app/src/main/java/com/codedifferently/lesson10/library/CivilianInterface.java
@@ -0,0 +1,9 @@
+package main.java.com.codedifferently.lesson10.library;
+
+
+ public interface Civilian {
+
+    void checkOutItem();
+    void returnItem();
+
+}
---
+++ lesson_10/solid/solid_app/src/main/java/com/codedifferently/lesson10/library/Librarian.java
@@ -0,0 +1,21 @@
+package main.java.com.codedifferently.lesson10.library;
+
+
+public class Librarian implements Civilian{
+
+
+    @Override
+    public void checkOutBook() {
+        System.out.println( "Librarian is checking the book out");
+    }
+
+
+
+    public void returnBook() {
+        System.out.println( "Librarian is returning book");
+    }
+    
+    
+
+
+}
