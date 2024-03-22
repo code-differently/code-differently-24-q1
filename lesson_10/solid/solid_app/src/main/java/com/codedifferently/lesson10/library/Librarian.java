09535b9fb10afc6a59f918a3a2f9c0af8b61c3f4
javyenware <warejavyen@gmail.com>
2024-03-22 13:59:52 +0000

    feat: added Librarian.java file and the class for Librarian

---
+++ lesson_10/solid/solid_app/src/main/java/com/codedifferently/lesson10/library/Librarian.java
@@ -0,0 +1,19 @@
+package com.codedifferently.lesson10.library;
+
+// extends keyword creates a sublclass that inherits properties and behaviors fromthe superclass parent class. 
+public class Librarian extends Patron {
+    
+public Librarian(String randomName) {
+    super(name)
+    // super keyword is used to refer to the immediate superclass of a class.
+}
+
+// method to checkout item 
+public void checkOut(Patron patron, Media item){
+
+}
+
+public void checkIn(Media item) {
+    
+}
+}
