b44fb9c9a66e580815bbb40c2a5cb7d7ff64d2ea
Aaron2278 <143147068+Aaron2278@users.noreply.github.com>
2024-03-21 20:26:36 -0400

    feat: adds Aaron's LibraryLog ,Book, and Guest with test blocks (#261)

---
+++ lesson_09/oop/oop_app/src/main/java/com/codedifferently/lesson9/AaronSantiago/LibraryGuest.java
@@ -0,0 +1,42 @@
+package com.codedifferently.lesson9.library;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class LibraryGuest {
+  private String name;
+  private List<Book> checkedOutBooks;
+
+  public LibraryGuest(String name) {
+    this.name = name;
+    this.checkedOutBooks = new ArrayList<>();
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void checkOutBook(Book book) {
+    checkedOutBooks.add(book);
+  }
+
+  public boolean returnBook(Book book) {
+    return checkedOutBooks.remove(book);
+  }
+
+  public List<Book> getCheckedOutBooks() {
+    return checkedOutBooks;
+  }
+
+  public void displayGuestDetails() {
+    System.out.println("Guest Name: " + name);
+    System.out.println("Checked Out Books:");
+    for (Book book : checkedOutBooks) {
+      book.displayBookDetails();
+    }
+  }
+}
