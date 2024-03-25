7f15ffbfd034369800c5827afb75345067cb8fd6
Anthony D. Mays <anthonydmays@users.noreply.github.com>
2024-03-25 13:08:40 -0700

    feat: adds Lesson 12 documentation and assignment (#301)
    * chore: initialize lesson_12 content
    
    * feat: adds support for loading from file
    
    * chore: organizes interfaces, adds csv files.
    
    * chore: updates column names
    
    * chore: adds csv support
    
    * chore: adds doco and CSV loader class boilerplate.
    
    * chore: swaps string id with UUID.
    
    * chore: cleaning up the code.
    
    * chore: adds search capabilities and tests to confirm correctness.
    
    * chore: adds test and updates build actions.
    
    * feat: adds simple CLI for searching catalog.
    
    * chore: sets up tests and instructions.
    
    * feat: adds cli support for providing loader.
    
    * chore: refactors cli code into common package.
    
    * lint: ran spotless
    
    * docs: adds arg for running with custom data loader.
    
    * docs: fixes annotation spelling.

---
+++ lesson_12/io/io_app/src/main/java/com/codedifferently/lesson12/library/LibraryGuestBase.java
@@ -0,0 +1,73 @@
+package com.codedifferently.lesson12.library;
+
+import com.codedifferently.lesson12.library.exceptions.LibraryNotSetException;
+import com.codedifferently.lesson12.library.exceptions.WrongLibraryException;
+import java.util.Objects;
+import java.util.Set;
+
+/** Base implementation of a library guest. */
+public class LibraryGuestBase implements LibraryGuest {
+
+  private Library library;
+  private final String name;
+  private final String email;
+
+  public LibraryGuestBase(String name, String email) {
+    this.name = name;
+    this.email = email;
+  }
+
+  @Override
+  public void setLibrary(Library library) throws WrongLibraryException {
+    if (library != null && !library.hasLibraryGuest(this)) {
+      throw new WrongLibraryException(
+          "Patron " + this.getId() + " is not in library " + library.getId());
+    }
+    this.library = library;
+  }
+
+  @Override
+  public String getName() {
+    return this.name;
+  }
+
+  @Override
+  public String getEmail() {
+    return this.email;
+  }
+
+  @Override
+  public String getId() {
+    return this.email;
+  }
+
+  @Override
+  public Set<MediaItem> getCheckedOutMediaItems() throws LibraryNotSetException {
+    if (this.library == null) {
+      throw new LibraryNotSetException("Library not set for patron " + this.getId());
+    }
+    return this.library.getCheckedOutByGuest(this);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof LibraryGuestBase)) {
+      return false;
+    }
+    LibraryGuestBase guest = (LibraryGuestBase) o;
+    return Objects.equals(getId(), guest.getId());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getId());
+  }
+
+  @Override
+  public String toString() {
+    return "LibraryGuestBase{" + "id='" + getId() + '\'' + ", name='" + getName() + '\'' + '}';
+  }
+}
