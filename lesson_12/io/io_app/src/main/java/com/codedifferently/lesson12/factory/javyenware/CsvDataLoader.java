55168a6bfd03a25be0997b44b7fd0dcf58cbee76
javyenware <warejavyen@gmail.com>
2024-03-26 17:42:21 +0000

    fix: added java util for Array, List, and Map

--- lesson_12/io/io_app/src/main/java/com/codedifferently/lesson12/factory/javyenware/CsvDataLoader.java
+++ lesson_12/io/io_app/src/main/java/com/codedifferently/lesson12/factory/javyenware/CsvDataLoader.java
@@ -2,41 +2,46 @@ package com.codedifferently.lesson12.factory.javyenware;
 
 import com.codedifferently.lesson12.factory.LibraryCsvDataLoader;
 import com.codedifferently.lesson12.models.LibraryDataModel;
-
+import com.codedifferently.lesson12.models.MediaItemModel;
 import org.springframework.stereotype.Service;
 
-import com.codedifferently.lesson12.models.MediaItemModel;
-import com.fasterxml.jackson.databind.ser.std.StdArraySerializers;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class CsvDataLoader implements LibraryCsvDataLoader {
+
     @Override
     public LibraryDataModel loadData() {
         var model = new LibraryDataModel();
         model.mediaItems = new ArrayList<>(); // loading this from the file
-        model.guests = new ArrayList<>(); // also gets loaded from the original file 
+        model.guests = new ArrayList<>(); // also gets loaded from the original file
 
-        List<MediaItemModel> mediaItems = readmediaItems();
+        List<MediaItemModel> mediaItems = readMediaItems();
         List<LibraryGuestModel> guests = readGuests();
         Map<String, List<CheckoutModel>> checkoutByGuestEmail = getCheckedoutItems();
 
-        for (String guest : guest) {
+        for (LibraryGuestModel guest : guests) {
             var checkouts = checkoutByGuestEmail.get(guest.email);
-            if (checkouts ! null) {
+            if (checkouts != null) {
                 guest.checkoutByGuestEmail = checkouts;
             }
         }
 
-        return null;
+        return model;
     }
 
-    private List<MediaItemModel> readMediaItems(String path){
+    private List<MediaItemModel> readMediaItems(String path) {
         var items = new ArrayList<MediaItemModel>();
-            for (String row : readCsvfile(path)) {
-                var item = new MediaItemModel();
-
-            }
+        for (String row : readCsvfile(path)) {
+            var item = new MediaItemModel();
+            item.id = row.get();
+            item.title = row.get();
+            item.type = row.get();
+            items.add(item);
+        }
+        return items;
     }
 
 }
