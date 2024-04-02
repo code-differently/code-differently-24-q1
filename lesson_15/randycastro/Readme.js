c606af7940061d726a0cb0e77c4db706cd7a291e
RandyCastr0 <randyyc15@gmail.com>
2024-04-02 13:48:45 +0000

    chore: deleted index.js and update other file

--- lesson_15/randycastro/Readme.js
+++ lesson_15/randycastro/Readme.js
@@ -1,23 +1,18 @@
-const { error } = require('console')
 const http = require('http')
 const port = 3000
 
 const server = http.createServer(function(req, res){
 
-res.write( "Test")
-res.end
+res.write('Test')
+res.end()
 
 })
 
-server.listen(port,function(error){
+server.listen(port,function(error) {
 if (error) {
-    console.log("something went wrong", error)
+    console.log('Something went wrong', error)
 } else {
-    console.log("server is listening on port" + port)
+    console.log('Server is listening on port' + port)
 }
-
-
-
-
 
 })
