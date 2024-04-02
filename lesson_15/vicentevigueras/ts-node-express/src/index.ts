0d1c2752da1e1667b128b1c6d4758ba23b6fee05
Vicente Vigueras <vicente.viguerassilva@gmail.com>
2024-04-02 16:56:36 +0000

    chore: Formatting (again)

--- lesson_15/vicentevigueras/ts-node-express/src/index.ts
+++ lesson_15/vicentevigueras/ts-node-express/src/index.ts
@@ -8,9 +8,7 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-
 app.use(express.static('/workspaces/code-differently-24-q1/lesson_01/VicenteVigueras'));
-
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
