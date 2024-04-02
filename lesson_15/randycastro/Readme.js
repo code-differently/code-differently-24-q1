2248175c551760a1efb91f4890c6d072e3f28244
RandyCastr0 <randyyc15@gmail.com>
2024-04-02 14:30:07 +0000

    feat: added readme.html

--- lesson_15/randycastro/Readme.js
+++ lesson_15/randycastro/Readme.js
@@ -1,11 +1,19 @@
 const http = require('http')
+const fs = require ('fs')
+const { error } = require('console')
 const port = 3000
 
 const server = http.createServer(function(req, res){
-
-res.write('Test')
-res.end()
-
+res.writeHead(200, { 'Content-Type' : 'text/html'})
+fs.readFile ('readme.html', function (error, data) {
+     if(error) {
+      res.writeHead(404)
+     res.write( 'Error File Not Found')
+  } else {
+    res.write (data)
+  }
+    res.end()
+  })
 })
 
 server.listen(port,function(error) {
