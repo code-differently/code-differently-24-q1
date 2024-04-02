ccb1de6e290276a6ba44d021febca8f3a3771358
Vicente Vigueras <vicente.viguerassilva@gmail.com>
2024-04-02 16:50:59 +0000

    feat: index.ts now loads lesson01 html

--- lesson_15/vicentevigueras/ts-node-express/src/index.ts
+++ lesson_15/vicentevigueras/ts-node-express/src/index.ts
@@ -9,16 +9,7 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 
-// Set the directory containing your HTML and CSS files
-const publicDirectoryPath = path.join(__dirname, "/workspaces/code-differently-24-q1/lesson_01/VicenteVigueras");
-
-// Middleware to serve static files
-app.use(express.static(publicDirectoryPath));
-
-app.get("/", (req: Request, res: Response) => {
-  // Send the HTML file
-  res.sendFile(path.join(publicDirectoryPath, "index.html"));
-});
+app.use(express.static('/workspaces/code-differently-24-q1/lesson_01/VicenteVigueras'));
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
