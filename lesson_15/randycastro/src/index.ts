27a998ab06fdc7c1a48ece4c363a64d935904846
RandyCastr0 <randyyc15@gmail.com>
2024-04-03 20:09:27 +0000

    chore: rearange files w/ anthony

--- lesson_15/randycastro/src/index.ts
+++ lesson_15/randycastro/src/index.ts
@@ -10,9 +10,6 @@ const app: Express = express();
 app.use(express.static('public'))
 const port = process.env.PORT || 4000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
