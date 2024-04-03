a98b7e0d0e2edd7f930c001f8319522ed6b8151a
“Mekhi” <mekhi090903@gmail.comgit config --global user.email mekhi090903@gmail.com>
2024-04-03 13:38:10 +0000

    Feat: Added files for web server

---
+++ lesson_15/src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
