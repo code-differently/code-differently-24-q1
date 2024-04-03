529f100635053c8e69027eb891c1e4634b94e984
Nelshulla <Jadanicole0@gmail.com>
2024-04-03 03:20:44 +0000

    Revert "feat: implemented librarian, Magazine,Newspaper,Exceptions"
    This reverts commit be451c3b00f540ba8ab5f64e1627def44e134c7b.

--- lesson_10/solid/solid_app/src/main/java/com/codedifferently/lesson10/jadaslibrary/exceptions/WrongLibraryException.java
+++
@@ -1,7 +0,0 @@
-package com.codedifferently.lesson10.jadaslibrary.exceptions;
-
-public class WrongLibraryException extends RuntimeException {
-  public WrongLibraryException(String message) {
-    super(message);
-  }
-}
