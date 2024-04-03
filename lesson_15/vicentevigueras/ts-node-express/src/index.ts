21c60d1924fbc3bf238c27da87e2e19de0c865f7
Vicente Vigueras <vicente.viguerassilva@gmail.com>
2024-04-03 13:19:40 +0000

    feat: TypeScript now references a directory inside lesson15

--- lesson_15/vicentevigueras/ts-node-express/src/index.ts
+++ lesson_15/vicentevigueras/ts-node-express/src/index.ts
@@ -1,14 +1,14 @@
 // src/index.js
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import path from "path";
+
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('/workspaces/code-differently-24-q1/lesson_01/VicenteVigueras'));
+app.use(express.static('public'));
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
