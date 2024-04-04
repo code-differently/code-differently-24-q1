751d172574441fe5d6b1a3dfa614df964de6ca41
Vicente Vigueras <vicente.viguerassilva@gmail.com>
2024-04-04 19:40:05 +0000

    chore: Uploading outline for MediaController

--- lesson_16/api/api_app/src/main/java/com/codedifferently/lesson16/web/MediaItemsController.java
+++ lesson_16/api/api_app/src/main/java/com/codedifferently/lesson16/web/MediaItemsController.java
@@ -4,12 +4,24 @@ import com.codedifferently.lesson16.library.Librarian;
 import com.codedifferently.lesson16.library.Library;
 import com.codedifferently.lesson16.library.MediaItem;
 import com.codedifferently.lesson16.library.search.SearchCriteria;
+
+import jakarta.validation.Valid;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *
+ * @author vscode
+ */
 @RestController
 public class MediaItemsController {
   private final Library library;
@@ -26,5 +38,18 @@ public class MediaItemsController {
     List<MediaItemResponse> responseItems = items.stream().map(MediaItemResponse::from).toList();
     var response = GetMediaItemsResponse.builder().items(responseItems).build();
     return response;
+  }
+
+  @PostMapping("/items")
+  public CreateMediaItemResponse postItem(@Valid @RequestBody CreateMediaItemRequest request) {
+    MediaItem item = MediaItemRequest.asMediaItem(request.getItem());
+    library.addMediaItem(item, librarian);
+    return CreateMediaItemResponse.builder().item(MediaItemResponse.from(item)).build();
+  }
+
+  @GetMapping("/items/{id}")
+  public GetMediaItemsResponse getItemById(@PathVariable String id) {
+  MediaItem item = MediaItemRequest.asMediaItem();
+  return CreateMediaItemResponse.builder().item(MediaItemResponse.from(item)).build();
   }
 }
