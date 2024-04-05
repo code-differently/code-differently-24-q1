79aff0689dd9b1cbb5f13daf7ca6ca5e8b2f54c5
Vicente Vigueras <vicente.viguerassilva@gmail.com>
2024-04-05 16:50:49 +0000

    feat: started mediaItemById method utilizing @GetMapping

--- lesson_16/api/api_app/src/main/java/com/codedifferently/lesson16/web/MediaItemsController.java
+++ lesson_16/api/api_app/src/main/java/com/codedifferently/lesson16/web/MediaItemsController.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
