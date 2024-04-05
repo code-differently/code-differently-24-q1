70f58d74c94279807f00c39b5afb815b48a08657
Vicente Vigueras <vicente.viguerassilva@gmail.com>
2024-04-05 20:05:47 +0000

    feat: deleteMediaItem Implementation

--- lesson_16/api/api_app/src/main/java/com/codedifferently/lesson16/web/MediaItemsController.java
+++ lesson_16/api/api_app/src/main/java/com/codedifferently/lesson16/web/MediaItemsController.java
@@ -4,15 +4,13 @@ import com.codedifferently.lesson16.library.Librarian;
 import com.codedifferently.lesson16.library.Library;
 import com.codedifferently.lesson16.library.MediaItem;
 import com.codedifferently.lesson16.library.search.SearchCriteria;
-
 import jakarta.validation.Valid;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
-
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,11 +45,17 @@ public class MediaItemsController {
     return CreateMediaItemResponse.builder().item(MediaItemResponse.from(item)).build();
   }
 
-  @GetMapping("/items/{id}")
-  public ResponseEntity<MediaItemResponse> getMediaItem(@PathVariable UUID id) {
-   SearchCriteria criteria = SearchCriteria.builder.id(id.toString().build());
-   Set<MediaItem> 
-   
-   
+  // @GetMapping("/items")
+  // public GetMediaItemsResponse getSingleItem() {
+  //   Set<MediaItem> items = library.search(SearchCriteria.builder().build());
+  //   List<MediaItemResponse> responseItems = items.stream().map(MediaItemResponse::from).toList();
+  //   var response = GetMediaItemsResponse.builder().items(responseItems).build();
+  //   return response;
+  // }
+
+  @DeleteMapping("/item/{id}")
+  public ResponseEntity<Void> deleteMediaItem(@PathVariable("id") UUID id) {
+    library.removeMediaItem(id, librarian);
+    return ResponseEntity.noContent().build();
   }
 }
