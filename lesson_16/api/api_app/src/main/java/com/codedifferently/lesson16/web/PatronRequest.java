511ab7e31c2e621d43b84dba4a6e61f76f511a08
RichHawkins <richhawkins1986@gmail.com>
2024-04-05 21:44:10 +0000

    chore:refactoring code

--- lesson_16/api/api_app/src/main/java/com/codedifferently/lesson16/web/PatronRequest.java
+++ lesson_16/api/api_app/src/main/java/com/codedifferently/lesson16/web/PatronRequest.java
@@ -1,1 +1,25 @@
+package com.codedifferently.lesson16.web;
 
+import java.util.UUID;
+
+import com.codedifferently.lesson16.library.Patron;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PatronRequest {
+    private UUID id;
+    private String name;
+    private String email;
+
+  public static Patron asPatron(PatronRequest request) {
+    return new Patron (request.getName(), request.getEmail());
+    
+}
+}
