ed6df4fdb37a5df91e956bd7f887f3d70a358822
RichHawkins <richhawkins1986@gmail.com>
2024-04-05 15:32:45 +0000

    chore:adds PatronResponse file and functionality

---
+++ lesson_16/api/api_app/src/main/java/com/codedifferently/lesson16/web/PatronResponse.java
@@ -0,0 +1,28 @@
+package com.codedifferently.lesson16.web;
+
+import java.util.UUID;
+
+import com.codedifferently.lesson16.library.Patron;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class PatronResponse {
+
+    private UUID id;
+    private String name;
+    private String email;
+
+    /**
+     * Converts a Patron object to a PatronResponse object.
+     *
+     * @param patron The Patron object to convert.
+     * @return The converted PatronResponse object.
+     */
+  public static PatronResponse from(Patron patron) {
+    return PatronResponse.builder().id(patron.getId()).name(patron.getName()).email(patron.getEmail()).build();
+
+  }
+}
