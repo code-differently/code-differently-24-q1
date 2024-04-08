7eb5a3d812e11d60c092046cd1045550e1c9ca8c
Vicente Vigueras <vicente.viguerassilva@gmail.com>
2024-04-08 16:06:55 +0000

    feat: CreatePatronResponse, CreatePatronRequest, GetPatronsResponse classes outline

---
+++ lesson_16/api/api_app/src/main/java/com/codedifferently/lesson16/web/GetPatronsResponse.java
@@ -0,0 +1,12 @@
+package com.codedifferently.lesson16.web;
+
+import java.util.List;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Singular;
+
+@Data
+@Builder
+public class GetPatronsResponse {
+  @Singular private List<PatronsResponse> patrons;
+}
