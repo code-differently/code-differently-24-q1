15bf19b5d33a639356903ef205e953fadced0a76
javyenware <warejavyen@gmail.com>
2024-04-08 13:57:53 +0000

    added: IOException and RestController import

--- lesson_16/api/api_app/src/main/java/com/codedifferently/lesson16/web/PatronsController.java
+++ lesson_16/api/api_app/src/main/java/com/codedifferently/lesson16/web/PatronsController.java
@@ -1,21 +1,32 @@
 package com.codedifferently.lesson16.web;
 
+import java.io.IOException;
+
 import org.apache.tomcat.jni.Library;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 
-
+@RestController
 public class PatronsController {
     private final Library library;
-    private final 
     
 
-    public PatronsController(Library library) {
+    public PatronsController(Library library) throws IOException {
         this.library = library;
+
     }
+
     @GetMapping("list of patrons")
     public GetPatronsController getListOfPatrons() {
         
     }
+
+
+
+
+
+
+
 }
