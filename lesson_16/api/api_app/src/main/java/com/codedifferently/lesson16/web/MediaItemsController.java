83d6ecab7b2dc489ccb410989c9c2e5e7e615daa
SherllinT <s.torres91@gmail.com>
2024-04-08 15:50:39 +0000

    feat: MediaItemsController, missing http status request

--- lesson_16/api/api_app/src/main/java/com/codedifferently/lesson16/web/MediaItemsController.java
+++ lesson_16/api/api_app/src/main/java/com/codedifferently/lesson16/web/MediaItemsController.java
@@ -7,7 +7,15 @@ import com.codedifferently.lesson16.library.search.SearchCriteria;
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,6 +33,34 @@ public class MediaItemsController {
     Set<MediaItem> items = library.search(SearchCriteria.builder().build());
     List<MediaItemResponse> responseItems = items.stream().map(MediaItemResponse::from).toList();
     var response = GetMediaItemsResponse.builder().items(responseItems).build();
+    return response;
+  }
+
+  @GetMapping("/items/{id}")
+  public GetMediaItemsResponse getItem() {
+    Set<MediaItem> items = library.search(SearchCriteria.builder().build());
+    List<MediaItemResponse> responseItems = items.stream().map(MediaItemResponse::from).toList();
+    var response = GetMediaItemsResponse.builder().items(responseItems).build();
+    return response;
+  }
+
+  @DeleteMapping("/items/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public ResponseEntity<Void> deleteItem(@PathVariable("id") UUID id) {
+    if (!library.hasMediaItem(id)) {
+      return ResponseEntity.notFound().build();
+    }
+    library.removeMediaItem(id, librarian);
+    return ResponseEntity.noContent().build();
+  }
+
+  @PostMapping("/items")
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public CreateMediaItemResponse createItem(@RequestBody CreateMediaItemRequest request) {
+    MediaItem item = MediaItemRequest.asMediaItem(request.getItem());
+    library.addMediaItem(item, librarian);
+    var response = CreateMediaItemResponse.builder().item(MediaItemResponse.from(item)).build();
+
     return response;
   }
 }
