92563ecb89aaad79ae874c8411f5087d322bafba
kjknows <kmase1022@gmail.com>
2024-04-08 15:48:57 +0000

    feat:adding mediaItemController

--- lesson_16/api/api_app/src/main/java/com/codedifferently/lesson16/web/MediaItemsController.java
+++ lesson_16/api/api_app/src/main/java/com/codedifferently/lesson16/web/MediaItemsController.java
@@ -1,14 +1,10 @@
 package com.codedifferently.lesson16.web;
 
-import com.codedifferently.lesson16.library.Librarian;
-import com.codedifferently.lesson16.library.Library;
-import com.codedifferently.lesson16.library.MediaItem;
-import com.codedifferently.lesson16.library.search.SearchCriteria;
-import jakarta.validation.Valid;
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,6 +14,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+
+import com.codedifferently.lesson16.library.Librarian;
+import com.codedifferently.lesson16.library.Library;
+import com.codedifferently.lesson16.library.MediaItem;
+import com.codedifferently.lesson16.library.search.SearchCriteria;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class MediaItemsController {
@@ -36,6 +39,7 @@ public class MediaItemsController {
     var response = GetMediaItemsResponse.builder().items(responseItems).build();
     return response;
   }
+  
 
   @PostMapping("/items")
   public CreateMediaItemResponse postItem(@Valid @RequestBody CreateMediaItemRequest request) {
