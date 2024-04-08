06db9c3630e414e5f1d561219a7d50c76f081da1
Vicente Vigueras <vicente.viguerassilva@gmail.com>
2024-04-08 16:23:49 +0000

    chore: PatronsController outline

--- lesson_16/api/api_app/src/main/java/com/codedifferently/lesson16/web/PatronsController.java
+++ lesson_16/api/api_app/src/main/java/com/codedifferently/lesson16/web/PatronsController.java
@@ -1,6 +1,17 @@
 package com.codedifferently.lesson16.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PatronsController {}
+@RequestMapping("/patrons")
+public class PatronsController {
+  // TODO
+  // Implement Test Cases
+
+  // @GetMapping
+  // @PostMapping
+  // @GetMapping("/{id}")
+  // @DeleteMapping("/{id}")
+
+}
