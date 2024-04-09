b1a5d8f5d30d881b1e8daca97f3d1fd97ecedf96
Anthony D. Mays <anthonydmays@users.noreply.github.com>
2024-04-08 21:41:50 -0700

    feat: adds Lesson 18 content and exercise walkthrough (#373)
    * chore: adding boilerplate and templates.
    
    * chore: update the readme
    
    * docs: adds walkthrough tutorial
    
    * doc: fixing instructions
    
    * doc: nit updates
    
    * docs: adds a note about CORS.
    
    * doc: adds alternative dev tools shortcut
    
    * docs: nit

---
+++ lesson_18/api/api_app/src/main/java/com/codedifferently/lesson18/models/MediaItemModel.java
@@ -0,0 +1,29 @@
+package com.codedifferently.lesson18.models;
+
+import com.codedifferently.lesson18.library.MediaType;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "media_items")
+public class MediaItemModel {
+  @Enumerated(EnumType.STRING)
+  public MediaType type;
+
+  @Id public UUID id;
+  public String isbn;
+  public String title;
+
+  @Convert(converter = AuthorsConverter.class)
+  public List<String> authors;
+
+  public String edition;
+  public Integer pages = 0;
+  public Integer runtime = 0;
+}
