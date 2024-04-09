38f11c7414607f69d2ee7be7e5f1b7b15e8e8c29
RandyCastr0 <randyyc15@gmail.com>
2024-04-09 14:57:02 +0000

    chore: deleted fornt  foward slash

--- lesson_16/api/api_app/src/main/java/com/codedifferently/lesson16/web/MediaItemsController.java
+++ lesson_16/api/api_app/src/main/java/com/codedifferently/lesson16/web/MediaItemsController.java
@@ -1,10 +1,13 @@
 package com.codedifferently.lesson16.web;
 
+import com.codedifferently.lesson16.library.Librarian;
+import com.codedifferently.lesson16.library.Library;
+import com.codedifferently.lesson16.library.MediaItem;
+import com.codedifferently.lesson16.library.search.SearchCriteria;
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,11 +17,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-
-import com.codedifferently.lesson16.library.Librarian;
-import com.codedifferently.lesson16.library.Library;
-import com.codedifferently.lesson16.library.MediaItem;
-import com.codedifferently.lesson16.library.search.SearchCriteria;
 
 @RestController
 public class MediaItemsController {
@@ -57,11 +55,11 @@ public class MediaItemsController {
     return ResponseEntity.noContent().build();
   }
 
-  @PostMapping("/items/")
+  @PostMapping("/items")
   public CreateMediaItemResponse postItem(@RequestBody CreateMediaItemRequest request) {
     MediaItem item = MediaItemRequest.asMediaItem(request.getItem());
     library.addMediaItem(item, librarian);
     var response = CreateMediaItemResponse.builder().item(MediaItemResponse.from(item)).build();
     return response;
-}
+  }
 }
