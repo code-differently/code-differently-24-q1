c3c288583b269c1f8ee725f36376eb23866dc9ee
RandyCastr0 <randyyc15@gmail.com>
2024-04-10 20:32:59 +0000

    chore: added try catch for delete item and completed Not Found exception

--- lesson_16/api/api_app/src/main/java/com/codedifferently/lesson16/web/MediaItemsController.java
+++ lesson_16/api/api_app/src/main/java/com/codedifferently/lesson16/web/MediaItemsController.java
@@ -1,13 +1,10 @@
 package com.codedifferently.lesson16.web;
 
-import com.codedifferently.lesson16.library.Librarian;
-import com.codedifferently.lesson16.library.Library;
-import com.codedifferently.lesson16.library.MediaItem;
-import com.codedifferently.lesson16.library.search.SearchCriteria;
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,6 +14,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+
+import com.codedifferently.lesson16.library.Librarian;
+import com.codedifferently.lesson16.library.Library;
+import com.codedifferently.lesson16.library.MediaItem;
+import com.codedifferently.lesson16.library.search.SearchCriteria;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class MediaItemsController {
@@ -51,12 +55,20 @@ public class MediaItemsController {
 
   @DeleteMapping("/items/{id}")
   public ResponseEntity<Void> deleteItem(@PathVariable("id") UUID id) {
-    library.removeMediaItem(id, librarian);
-    return ResponseEntity.noContent().build();
+    try {
+      library.removeMediaItem(id, librarian);
+      return ResponseEntity.noContent().build();
+    } catch (Exception e) {
+      return ResponseEntity.notFound().build();
+    }
   }
 
+  /**
+   * @param request
+   * @return
+   */
   @PostMapping("/items")
-  public CreateMediaItemResponse postItem(@RequestBody CreateMediaItemRequest request) {
+  public CreateMediaItemResponse postItem(@Valid @RequestBody CreateMediaItemRequest request) {
     MediaItem item = MediaItemRequest.asMediaItem(request.getItem());
     library.addMediaItem(item, librarian);
     var response = CreateMediaItemResponse.builder().item(MediaItemResponse.from(item)).build();
