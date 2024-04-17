5cb0cbef699fc0d1bf6d27575ddb4832d11f6797
Anthony D. Mays <anthony@morganlatimer.com>
2024-04-17 05:09:08 +0000

    feat: establish initial layout

--- lesson_22/README.md
+++ lesson_22/README.md
@@ -1,1 +1,11 @@
 # Lesson 22
+
+## Homework
+
+## Introduction to React
+
+1. Install the API server using `npm install` and then run it using the `npm run dev` command. 
+2. Install the React web server using `npm install` and then run it using the `npm run dev` command.
+3. Create React components for the Code Differently website using the html provided to you in the [html](./html) folder.
+4. For the list of programs, use the [api](./api/src/server.ts) to dynamically read and render the list of available programs.
+5. Lastly, implement a new page that implements a form for adding a new program. On form submit, return to the home page.
--- lesson_22/template/src/main.tsx
+++ lesson_22/template/src/main.tsx
@@ -1,11 +1,31 @@
 import App from './App.tsx';
+import {Home} from './pages/Home/Home.tsx';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
 
-import './index.css';
+import './index.scss';
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
